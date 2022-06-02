01dff6dc6ce2a3efeb966b62d351328eaee33f29
RochaThomas <tomrocha99@gmail.com>
2022-06-02 11:40:15 -0700

    morning algos finished median sliding window

--- slidingWindowMedian.js
+++ slidingWindowMedian.js
@@ -6,7 +6,6 @@
 class Node {
     constructor(data){
         this.data = data;
-        this.position = null;
         this.next = null;
         this.prev = null;
     }
@@ -30,15 +29,17 @@ class Queue {
         if (this.isEmpty()){
             this.front = newNode;
             this.rear = newNode;
-            newNode.position = 0;
         }
         else {
             this.rear.next = newNode;
             newNode.prev = this.rear;
             this.rear = newNode;
-            newNode.position = newNode.prev.position + 1;
-
         }
+    }
+
+    dequeue(){
+        this.front = this.front.next;
+        this.front.prev = null;
     }
 
     //fix the sorting method...results in infinite loop
@@ -49,23 +50,6 @@ class Queue {
         let runner = this.front;
         
         while (runner.next){
-            let back = runner.next;
-            let val = runner.next;
-            while (val){
-                if (val.prev === null || back.data > val.prev.data){
-                    const temp = back.position;
-                    back.position = val.position;
-                    val.position = temp;
-                    break;
-                }
-                else {
-                    val = val.prev;
-                }
-            }
-            console.log();
-            this.printQueue();
-            console.log();
-            runner = runner.next;
         }
         
         console.log();
@@ -74,55 +58,103 @@ class Queue {
 
     // helper function
     printQueue() {
-        // var runner = this.front;
-        // if (this.front) {
-        //     console.log("FRONT: " + this.front.data)
-        // }
+        var runner = this.front;
+        if (this.front) {
+            console.log("FRONT: " + this.front.data)
+        }
 
-        // while (runner) {
-        //     console.log(runner.data)
-        //     runner = runner.next
-        // }
-        // if (this.rear) {
-        //     console.log("REAR: " + this.rear.data)
-        // }
+        while (runner) {
+            console.log(runner.data)
+            runner = runner.next
+        }
+        if (this.rear) {
+            console.log("REAR: " + this.rear.data)
+        }
+    }
 
+
+    //finding the median of the window
+    findMedian(){
+        const temp = [];
         let runner = this.front;
-
         while (runner){
-            console.log("data:", runner.data, " ---- position:", runner.position);
+            temp.push(runner.data);
             runner = runner.next;
         }
 
+        // temp.sort();
+
+        for (let i = 0; i < temp.length; i++){
+            for (let j = 0; j < temp.length - i - 1; j++){
+                if (temp[j] > temp[j+1]){
+                    [temp[j],temp[j+1]] = [temp[j+1],temp[j]];
+                }
+            } 
+        }
+
+        // console.log("WINDOW: ",temp);
+        if (temp.length % 2 === 0){
+            let average = null;
+            average = (temp[temp.length/2 - 1] + temp[temp.length/2]) / 2;
+            return average;
+        }
+        else {
+            return temp[Math.floor(temp.length/2)];
+        }
     }
 }
 
-// function slidingWindowMedian(nums, k){
-//     let output = [];
-//     if (k % 2 === 0){
-//         const shift = k / 2 - 1;
-//         for (let i = shift; i < nums.length - shift - 1; i++){
-//             output.push((nums[i] + nums[i + 1]) / 2);
-//         }
-//     }
-//     else {
-//         const shift = Math.floor(k/2);
-//         for (let i = shift; i < nums.length - shift; i++){
-//             output.push(nums[i]);
-//         }
-//     }
-//     return output;
-// }
+/*
+get the array
+for loop through the array
+enqueue first size
+sort : either .sort or make your own sort
+find median : if odd get the middle, if even average the middle two
+output median
+dequeue front and enqueue next in array
 
-// console.log(slidingWindowMedian([1,3,-1,-3,5,3,6,7], 3));
+Window position                Median
+---------------                -----
+[1  3  -1] -3  5  3  6  7        1
+ 1 [3  -1  -3] 5  3  6  7       -1
+ 1  3 [-1  -3  5] 3  6  7       -1
+ 1  3  -1 [-3  5  3] 6  7        3
+ 1  3  -1  -3 [5  3  6] 7        5
+ 1  3  -1  -3  5 [3  6  7]       6
 
-var window = new Queue();
+ for( init queue with size k )
 
-window.enqueue(1);
-window.enqueue(3);
-window.enqueue(-1);
-window.enqueue(-3);
-window.enqueue(5);
-// window.printQueue();
+ for( traversing nums array )
 
-window.sort();
+*/
+
+function slidingWindowMedian(arr, size){
+    
+    const window = new Queue();
+    const output = [];
+
+    //initializing the original window
+    for (let i = 0; i < size; i++){
+        window.enqueue(arr[i]);
+    }
+    
+    //output median intial window
+    output.push(window.findMedian())
+
+    for (let j = size; j < arr.length; j++){
+        window.dequeue();
+        window.enqueue(arr[j]);
+        output.push(window.findMedian());
+    }
+
+    return output;
+}
+
+
+
+// Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+// Input: nums = [1,2,3,4,2,3,1,4,2], k = 3
+
+console.log(slidingWindowMedian([1,3,-1,-3,5,3,6,7], 4));
+
+// slidingWindowMedian([1,2,3,4,2,3,1,4,2], 4);
