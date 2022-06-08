c64c50192b9c798c47b04a2978db268847e2a0e4
RochaThomas <tomrocha99@gmail.com>
2022-06-08 09:32:30 -0700

    morning algos better group anagram solution

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -13,48 +13,65 @@
 
 # works but leetcode time limit exceeded
 
+# def groupAnagrams(strs):
+#     output = []
+
+#     x = 0
+
+#     while x < len(strs):
+#         letter_dict = {}
+#         group = []
+        
+#         for letter in strs[x]:
+#             if letter in letter_dict:
+#                 letter_dict[letter] += 1
+#             else:
+#                 letter_dict[letter] = 1
+        
+#         group.append(strs[x])
+#         strs.remove(strs[x])
+
+#         i = 0
+
+#         while i < len(strs):
+#             isAnagram = True
+#             temp_dict = letter_dict.copy()
+#             for letter in strs[i]:
+#                 if letter in temp_dict:
+#                     temp_dict[letter] -= 1
+#                 else:
+#                     isAnagram = False
+#                     break
+
+#             if any(temp_dict.values()):
+#                 isAnagram = False
+            
+#             if isAnagram == True:
+#                 group.append(strs[i])
+#                 strs.remove(strs[i])
+#             else:
+#                 i += 1
+        
+#         output.append(group)
+
+
+#     return output
+
+# more optimal solution
 def groupAnagrams(strs):
     output = []
+    sorted_letter_dict = {}
 
-    x = 0
-
-    while x < len(strs):
-        letter_dict = {}
-        group = []
-        
-        for letter in strs[x]:
-            if letter in letter_dict:
-                letter_dict[letter] += 1
-            else:
-                letter_dict[letter] = 1
-        
-        group.append(strs[x])
-        strs.remove(strs[x])
-
-        i = 0
-
-        while i < len(strs):
-            isAnagram = True
-            temp_dict = letter_dict.copy()
-            for letter in strs[i]:
-                if letter in temp_dict:
-                    temp_dict[letter] -= 1
-                else:
-                    isAnagram = False
-                    break
-
-            if any(temp_dict.values()):
-                isAnagram = False
-            
-            if isAnagram == True:
-                group.append(strs[i])
-                strs.remove(strs[i])
-            else:
-                i += 1
-        
-        output.append(group)
-
-
+    for word in strs:
+        sortedWord = "".join(sorted(word))
+        if sortedWord not in sorted_letter_dict:
+            sorted_letter_dict[sortedWord] = [word]
+        else:
+            sorted_letter_dict[sortedWord].append(word)
+    
+    for val in sorted_letter_dict.values():
+        output.append(val)
+    
     return output
 
 print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
