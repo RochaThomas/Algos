32ecba5e33162b680e38d122b977f244544f5c33
RochaThomas <tomrocha99@gmail.com>
2022-06-09 18:24:27 -0700

    morning algos incomplete top k freq

---
+++ topKFrequentElements.py
@@ -0,0 +1,42 @@
+# morning algos
+# top k frequent elements
+# neet code 150 #5
+
+def topKFrequent(nums, k):
+    
+    output = []
+    num_dict = {}
+    
+    nums = sorted(nums)
+    count = 0
+    last_num = nums[0]
+
+    # must use i in range
+
+    for num in nums:
+        if num == last_num:
+            count+=1
+        print(nums.index(num))
+        if num != last_num or nums.index(num) + 1 >= len(nums) - 1:
+            num_dict[last_num] = count
+            last_num = num
+            count = 1
+
+        if nums.index(num) == len(nums) - 1:
+            num_dict[nums[nums.index(num)]] = 1
+    
+    freq_array = sorted(num_dict.values(), reverse=True)
+
+    i = 0
+
+    # while i in range(k):
+    #     print(i)
+    #     if i == 0 or freq_array[i] != freq_array[i-1]:
+    #         for num in num_dict:
+    #             if num_dict[num] == freq_array[i]:
+    #                 output.append(num)
+    #     i += 1
+
+    return num_dict
+
+print(topKFrequent([1,1,1,2,2,2,3,3,3], 3))
