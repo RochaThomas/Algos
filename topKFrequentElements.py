9b73d7c3d7d147e8ce3d229e5d56401a2152a1f0
RochaThomas <tomrocha99@gmail.com>
2022-06-10 22:27:44 -0700

    morning algos incomplete

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -12,31 +12,43 @@ def topKFrequent(nums, k):
     last_num = nums[0]
 
     # must use i in range
+    # for num in nums:
+    #     if num == last_num:
+    #         count+=1
+    #     print(nums.index(num))
+    #     if num != last_num or nums.index(num) + 1 >= len(nums) - 1:
+    #         num_dict[last_num] = count
+    #         last_num = num
+    #         count = 1
 
-    for num in nums:
-        if num == last_num:
-            count+=1
-        print(nums.index(num))
-        if num != last_num or nums.index(num) + 1 >= len(nums) - 1:
+    #     if nums.index(num) == len(nums) - 1:
+    #         num_dict[nums[nums.index(num)]] = 1
+
+    x = 0
+    
+    while x < len(nums):
+        if nums[x] == last_num:
+            count += 1
+
+        if nums[x] != last_num or x+1 > len(nums) - 1:
             num_dict[last_num] = count
-            last_num = num
+            last_num = nums[x]
             count = 1
+        x += 1
 
-        if nums.index(num) == len(nums) - 1:
-            num_dict[nums[nums.index(num)]] = 1
     
     freq_array = sorted(num_dict.values(), reverse=True)
 
     i = 0
 
-    # while i in range(k):
-    #     print(i)
-    #     if i == 0 or freq_array[i] != freq_array[i-1]:
-    #         for num in num_dict:
-    #             if num_dict[num] == freq_array[i]:
-    #                 output.append(num)
-    #     i += 1
+    while i in range(k):
+        print(i)
+        if i == 0 or freq_array[i] != freq_array[i-1]:
+            for num in num_dict:
+                if num_dict[num] == freq_array[i]:
+                    output.append(num)
+        i += 1
 
-    return num_dict
+    return output
 
-print(topKFrequent([1,1,1,2,2,2,3,3,3], 3))
+print(topKFrequent([1,2], 2))
