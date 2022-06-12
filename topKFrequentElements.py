f13ef301e631514b35b34d3c0416751e6a6b9b20
RochaThomas <tomrocha99@gmail.com>
2022-06-12 14:26:21 -0700

    morning algos finally complete k most freq

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -11,44 +11,39 @@ def topKFrequent(nums, k):
     count = 0
     last_num = nums[0]
 
-    # must use i in range
-    # for num in nums:
-    #     if num == last_num:
-    #         count+=1
-    #     print(nums.index(num))
-    #     if num != last_num or nums.index(num) + 1 >= len(nums) - 1:
-    #         num_dict[last_num] = count
-    #         last_num = num
-    #         count = 1
-
-    #     if nums.index(num) == len(nums) - 1:
-    #         num_dict[nums[nums.index(num)]] = 1
-
     x = 0
     
     while x < len(nums):
         if nums[x] == last_num:
             count += 1
-
-        if nums[x] != last_num or x+1 > len(nums) - 1:
+        else:
             num_dict[last_num] = count
             last_num = nums[x]
             count = 1
+
+        if x+1 >= len(nums):
+            num_dict[last_num] = count
         x += 1
 
     
     freq_array = sorted(num_dict.values(), reverse=True)
 
     i = 0
+    x = 0
 
-    while i in range(k):
-        print(i)
-        if i == 0 or freq_array[i] != freq_array[i-1]:
+    while i < len(freq_array) and x <= k:
+        if i == 0:
             for num in num_dict:
                 if num_dict[num] == freq_array[i]:
                     output.append(num)
+                    x += 1
+        elif freq_array[i] != freq_array[i-1]:
+            for num in num_dict:
+                if num_dict[num] == freq_array[i]:
+                    output.append(num)
+                    x += 1
         i += 1
 
     return output
 
-print(topKFrequent([1,2], 2))
+print(topKFrequent([1], 1))
