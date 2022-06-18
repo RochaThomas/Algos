7e3029a31282c8d5c8808d3528f8ec292fbd4dbc
RochaThomas <tomrocha99@gmail.com>
2022-06-18 14:10:11 -0700

    morning algos twoSumII complete

--- twoSUmII.py
+++ twoSUmII.py
@@ -1,5 +1,25 @@
 # morning algos
 # neet code Two Sum II
 
+# works but its not fast enough with large entries
+# def twoSum(numbers, target):
+#     front = 0
+#     back = len(numbers) - 1
+
+#     while back > 0:
+#         if numbers[front] + numbers[back] != target:
+#             front += 1
+#         else:
+#             return [front + 1, back + 1]
+        
+#         if front == back:
+#             front = 0
+#             back -= 1
+
+
+# second optimized try
 def twoSum(numbers, target):
     pass
+
+print(twoSum([2,7,11,15], 9))
+    
