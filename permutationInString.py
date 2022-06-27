cd6495ac3ededa3177a003e858b3b877cdb0a997
RochaThomas <tomrocha99@gmail.com>
2022-06-26 19:00:06 -0700

    morning algos permutation in string incomplete

--- permutationInString.py
+++ permutationInString.py
@@ -2,6 +2,10 @@
 # morning algos
 # neetcode permutation in string, check inclusion
 
+# failed to complete
+# neetcode solution was not very helpful
+# i think if i try again i can come up with a working more understandable solution to this algo
+
 def checkInclusion(s1, s2):
     # first turn s1 into a dictionary
     # run a window using l and r pointers through s2
@@ -9,6 +13,7 @@ def checkInclusion(s1, s2):
     # if something doesn't match like letter isnt in dict or too many
     # add s[l] back and increment l
     letter_dict = {}
+    output = False
     for i in range(len(s1)):
         if s1[i] not in letter_dict:
             letter_dict[s1[i]] = 1
@@ -20,20 +25,24 @@ def checkInclusion(s1, s2):
     l = 0
     for r in range(len(s2)):
         if s2[r] not in letter_dict:
-            if s2[l]  in letter_dict:
+            while s2[l] in letter_dict:
                 letter_dict[s2[l]] += 1
+                l += 1
             l += 1
             continue
         else:
             letter_dict[s2[r]] -= 1
         
         if not any(letter_dict.values()):
-            return True
+            print(letter_dict)
+            output = True
+            break
         elif letter_dict[s2[r]] < 0:
-            if s1[i] not in letter_dict:
-                letter_dict[s1[i]] = 1
+            if s2[l] in letter_dict:
+                letter_dict[s2[l]] += 1
             l += 1
+    print(letter_dict)
 
-    return False
+    return output
 
-print(checkInclusion("ab", "eidboaooo"))
+print(checkInclusion("adc", "dcda"))
