2772dfffd5cbe7912352867d94e773a1c707ff77
RochaThomas <tomrocha99@gmail.com>
2022-07-18 12:13:13 -0700

    morning algos valid parenthesis complete

--- validParenthesis.py
+++ validParenthesis.py
@@ -1,7 +1,24 @@
 # morning algos
 # neetcode valid parenthesis
 
+
+# neetcode solution
+# using an array as a stack
+
 def isValid(s):
-    pass
+    
+    stack = []
+    closeToOpen = {")":"(", "]":"[","}":"{"}
+
+    for c in s:
+        if c in closeToOpen:
+            if stack and stack[-1] == closeToOpen[c]:
+                stack.pop()
+            else:
+                return False
+        else:
+            stack.append(c)
+    
+    return True if not stack else False
 
 print(isValid("()"))
