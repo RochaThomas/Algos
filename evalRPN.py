298f63da0ca9c329262a06b193265d390353138d
RochaThomas <tomrocha99@gmail.com>
2022-07-18 14:59:23 -0700

    morning algos evalRPN complete

--- evalRPN.py
+++ evalRPN.py
@@ -1,7 +1,33 @@
 # morning algos
 # neetcode evaluate reverse polish notation
 
-def evalRPN(tokens):
-    pass
 
-print(evalRPN(["2","1","+","3","*"]))
+def evalRPN(tokens):
+    stack = []
+    operations = ["+", "-", "*", "/"]
+    
+    for t in tokens:
+        if t in operations:
+            temp = stack[-1]
+            stack.pop()
+            if t == "*":
+                temp = stack[-1] * temp
+            elif t == "/":
+                # line below was originally just int() without the float but leetcode didn't get the same answer without the float()
+                temp = int(float(stack[-1]) / temp)
+            elif t == "+":
+                temp = stack[-1] + temp
+            elif t == "-":
+                temp = stack[-1] - temp
+            stack.pop()
+            stack.append(temp)
+        else:
+            t = int(t)
+            stack.append(t)
+
+        print(stack)
+
+    return stack[-1]
+
+print(evalRPN(
+["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
