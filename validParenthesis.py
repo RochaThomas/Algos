b1af7a74a7626fdbbe77da2dfa78eef7872bbfb9
RochaThomas <tomrocha99@gmail.com>
2022-07-18 12:15:10 -0700

    morning algos valid Parenthesis follow up

--- validParenthesis.py
+++ validParenthesis.py
@@ -4,6 +4,10 @@
 
 # neetcode solution
 # using an array as a stack
+# furthering this...
+# follow up: how would you do this for different characters
+# for example... A is open and a is close, B is open and b is closed ---> through the rest of the alphabet
+# use a for loop to set up a hashmap of closed to open characters and run the below logic, this should work
 
 def isValid(s):
     
