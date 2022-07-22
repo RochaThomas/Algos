922758cb2d5ea22ed01da4f3b147844dd226aef8
RochaThomas <tomrocha99@gmail.com>
2022-07-22 16:05:26 -0700

    morning algos koko eating bananas complete

--- minEatingSpeed.py
+++ minEatingSpeed.py
@@ -4,9 +4,36 @@
 
 # pseudo
 # number k is related to the number of piles and the number of hours given
+# not sure...
+
+
+# neetcode solution
+# change it into a binary search problem
+# test all the value rates from the lowest (smallest pile) to the highest (largest pile)
+# instead of iterating through 1 by 1
+# use a binary search to find the min viable eating rate
+# run the binary until the very end
+
+import math
 
 
 def minEatingSpeed(piles, h):
-    pass
+    l, r  = 1, max(piles)
+    result = r
+
+    while l <= r:
+        k = (l + r) // 2
+        totalHours = 0
+        for p in piles:
+            totalHours += math.ceil(p/k)
+        
+        if totalHours > h:
+            l = k + 1
+        elif totalHours <= h:
+            result = k
+            r = k - 1
+
+
+    return result
 
 print(minEatingSpeed([3, 6, 7, 11], 8))
