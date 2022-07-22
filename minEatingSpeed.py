18ac5f72b0a2f14f44bb83ba5fc997a282a6bb94
RochaThomas <tomrocha99@gmail.com>
2022-07-21 23:42:21 -0700

    morning algos koko eating bananas

---
+++ minEatingSpeed.py
@@ -0,0 +1,12 @@
+# morning algos
+# neetcode koko eating bananas
+
+
+# pseudo
+# number k is related to the number of piles and the number of hours given
+
+
+def minEatingSpeed(piles, h):
+    pass
+
+print(minEatingSpeed([3, 6, 7, 11], 8))
