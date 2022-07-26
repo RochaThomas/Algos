1535fbfbfd7e637eea224d92152bd651d22fb4e5
RochaThomas <tomrocha99@gmail.com>
2022-07-26 14:29:05 -0700

    morning algos timekeyvaluestore complete

--- timeKeyValueStore.py
+++ timeKeyValueStore.py
@@ -4,13 +4,28 @@
 class TimeMap:
 
     def __init__(self):
-        pass
+        self.store = {} #key = string, value = [val, timestamp]
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        pass
+        if key not in self.store:
+            self.store[key] = []
+        self.store[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        pass 
+        result = ""
+        values = self.store.get(key, [])
+
+        # run binary search here
+        l, r = 0, len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:
+                result =  values[m][0]
+                l = m + 1
+            else:
+                r = m - 1
+                
+        return result
 
 
 # Your TimeMap object will be instantiated and called as such:
