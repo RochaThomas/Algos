b4d0ca9f294e3eb6b302bd3806efb4a4b8499ab1
RochaThomas <tomrocha99@gmail.com>
2022-07-26 13:18:05 -0700

    morning algos findMinSortedArray complete

--- findMinSortedArray.py
+++ findMinSortedArray.py
@@ -2,6 +2,22 @@
 # neetcode find min in rotated sorted array
 
 def findMin(nums):
-    pass
+    result = nums[0]
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        if nums[l] < nums[r]:
+            result = min(result, nums[l])
+            break
+
+        m = (l + r) // 2
+        result = min(result, nums[m])
+
+        if nums[m] >= nums[l]:
+            l = m + 1
+        else:
+            r = m - 1
+
+    return result
 
 print(findMin([3,4,5,1,2]))
