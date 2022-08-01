db008a1267e6baf8dcf17ddd2b3ad96ade13571e
RochaThomas <tomrocha99@gmail.com>
2022-07-31 22:58:26 -0700

    morning algos removeNthFromEnd

---
+++ removeNthFromEnd.py
@@ -0,0 +1,14 @@
+
+
+# morning algos
+# neetcode remove Nth node from End of list
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def removeNthFromEnd(head, n):
+        pass
+
+    print(removeNthFromEnd())
