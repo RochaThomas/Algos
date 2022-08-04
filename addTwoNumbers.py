54aeaacad0f2b99457f7ede18e1e7370f77a58d3
RochaThomas <tomrocha99@gmail.com>
2022-08-03 20:12:32 -0700

    morning algos add two numbers

---
+++ addTwoNumbers.py
@@ -0,0 +1,12 @@
+# morning algos
+# neetcode add two numbers
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def addTwoNumbers(l1, l2):
+        pass
+
+    print(addTwoNumbers([2,4,3], [5,6,4]))
