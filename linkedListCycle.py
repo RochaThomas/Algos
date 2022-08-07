f32e1b72626944d39a318e18c3db2c712cd2f5e7
RochaThomas <tomrocha99@gmail.com>
2022-08-07 00:06:31 -0700

    morning algos linked list cycle

---
+++ linkedListCycle.py
@@ -0,0 +1,14 @@
+# morning algos
+# neetcode linked list cycle
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def hasCycle(head):
+        pass
+
+    print(hasCycle([3,2,0,-4]))
