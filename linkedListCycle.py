bee9300662994894c990de4c861c138cab2fdea8
RochaThomas <tomrocha99@gmail.com>
2022-08-09 09:00:36 -0700

    morning algos linkedListCycle complete

--- linkedListCycle.py
+++ linkedListCycle.py
@@ -7,8 +7,31 @@ class ListNode:
         self.val = x
         self.next = None
 
+# works but there is actually a faster O(1) solution
 class Solution:
     def hasCycle(head):
-        pass
+        # prevNodes = {}
+
+        # runner = head
+        # while runner:
+        #     prevNodes[runner] = runner.next
+        #     if runner.next in prevNodes:
+        #         return True
+            
+        #     runner = runner.next
+        
+        # return False
+
+        # neetcode solution O(1)
+        s, f = head, head
+
+        while f and f.next:
+            s = s.next
+            f = f.next.next
+            if s == f:
+                return True
+
+        return False
+
 
     print(hasCycle([3,2,0,-4]))
