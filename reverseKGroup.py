f3c2a8bea91822c34cf83fc177ce28d1c5ea6d09
RochaThomas <tomrocha99@gmail.com>
2022-08-18 22:37:34 -0700

    morning algos reverse nodes in k-group

---
+++ reverseKGroup.py
@@ -0,0 +1,13 @@
+# morning algos
+# neetcode reverse nodes in k-group
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def reverseKGroup(self, head, k):
+        pass
+
+    print(reverseKGroup([1,2,3,4,5], 2))
