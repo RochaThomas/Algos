d7cdc29282bfffb0781e48c5c7db73706c5443bd
RochaThomas <tomrocha99@gmail.com>
2022-08-19 23:10:30 -0700

    morning algos invert binary tree

---
+++ invertBinaryTree.py
@@ -0,0 +1,13 @@
+# morning algos
+# neetcode invert binary tree
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def invertTree(self, root):
+        pass
+
+    print(invertTree([4,2,7,1,3,6,9]))
