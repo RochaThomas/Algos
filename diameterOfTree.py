cbf0e875f69d37c9f6b3abcff7ad7f9327271b07
RochaThomas <tomrocha99@gmail.com>
2022-08-22 16:37:42 -0700

    morning algos diameter of tree complete

--- diameterOfTree.py
+++ diameterOfTree.py
@@ -2,6 +2,9 @@
 # neetcode diameter of binary tree
 
 # Definition for a binary tree node.
+from cgitb import reset
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -9,6 +12,23 @@ class TreeNode:
         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root):
-        pass
+        res = [0]
+
+
+
+        def dfs(root):
+            if not root:
+                return -1
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            res[0] = max(res[0], 2 + left + right)
+
+            return 1 + max(left, right)
+        
+        dfs(root)
+
+        return res[0]
+
 
     print(diameterOfBinaryTree([1,2,3,4,5]))
