03b96e8ef755c952cca4c40dae14ec1855a3f133
RochaThomas <tomrocha99@gmail.com>
2022-08-22 15:40:13 -0700

    morning algos reverse k group complete

--- reverseKGroup.py
+++ reverseKGroup.py
@@ -8,6 +8,35 @@ class ListNode:
         self.next = next
 class Solution:
     def reverseKGroup(self, head, k):
-        pass
+        dummy = ListNode(0, head)
+        groupPrev = dummy
+
+        while True:
+            kth = self.getKth(groupPrev, k)
+            if not kth:
+                break
+            groupNext = kth.next
+
+            # reverse the group
+            prev, curr = kth.next, groupPrev.next
+            
+            while curr != groupNext:
+                temp = curr.next
+                curr.next = prev
+                prev = curr
+                curr = temp
+
+            temp = groupPrev.next
+            groupPrev.next = kth
+            groupPrev = temp
+
+        return dummy.next
+
+    def getKth(self, curr, k):
+        while curr and k > 0:
+            curr = curr.next
+            k -= 1
+
+        return curr
 
     print(reverseKGroup([1,2,3,4,5], 2))
