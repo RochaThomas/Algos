6d1e5e195ed58b39f14991f19134cb10671ced5a
RochaThomas <tomrocha99@gmail.com>
2022-08-24 23:34:01 -0700

    morning algos sameTree

---
+++ sameTree.py
@@ -0,0 +1,14 @@
+# morning algos
+# neetcode same tree
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def isSameTree(self, p, q):
+        pass
+
+    print(isSameTree([1,2,3], [1,2,3]))
