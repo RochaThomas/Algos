4223eeeff1a411e19ce32a3da9a257a18784baaf
RochaThomas <tomrocha99@gmail.com>
2022-08-26 22:50:36 -0700

    morning algos lowest common ancestor

---
+++ lowestCommonAncestor.py
@@ -0,0 +1,15 @@
+# morning algos
+# neetcode lowest common ancestor of a binary search tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root, p, q):
+        pass
+
+    print(lowestCommonAncestor([6,2,8,0,4,7,9,None,None,3,5], 2, 8))
