7e07b4fed725e222c50d4ff71289eb9354ba0cd7
RochaThomas <tomrocha99@gmail.com>
2022-08-28 23:23:52 -0700

    morning algos binary tree right side view

---
+++ rightSideView.py
@@ -0,0 +1,16 @@
+# morning algos
+# neetcode binary tree right side view
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def rightSideView(self, root):
+        pass
+
+    print(rightSideView([1,2,3,None,5,None,4]))
