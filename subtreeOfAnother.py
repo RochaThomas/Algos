30bc2fda9f8428095b4ab488fab936914831d20c
RochaThomas <tomrocha99@gmail.com>
2022-08-29 17:39:35 -0700

    morning algos subtree of another complete

--- subtreeOfAnother.py
+++ subtreeOfAnother.py
@@ -9,6 +9,25 @@
 #         self.right = right
 class Solution:
     def isSubtree(self, root, subRoot):
-        pass
+        if not subRoot:
+            return True
+        
+        # in reality this next line's edge case is really evaluating if not root and subroot
+        # but the first if statement already checked to make sure that subRoot is non null
+        if not root:
+            return False
+        if self.isSameTree(root, subRoot):
+            return True
+
+        return (self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot))
+
+    def isSameTree(self, s, t):
+        if not s and not t:
+            return True
+        if not s or not t or s.val != t.val:
+            return False
+        # comment
+        
+        return (self.isSameTree(s.left, t.left) and self.isSameTree(s.right, t.right))
 
     print(isSubtree([3,4,5,1,2], [4,1,2]))
