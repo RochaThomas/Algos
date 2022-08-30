bf29936b91c791e838744281ae7478541748a34b
RochaThomas <tomrocha99@gmail.com>
2022-08-29 17:12:21 -0700

    morning algos sameTree complete

--- sameTree.py
+++ sameTree.py
@@ -9,6 +9,11 @@ class TreeNode:
         self.right = right
 class Solution:
     def isSameTree(self, p, q):
-        pass
+        if not p and not q:
+            return True
+        if not p or not q or p.val != q.val:
+            return False
 
+        return (self.isSameTree(p.left, p.right) and self.isSameTree(q.left, q.right))
+        
     print(isSameTree([1,2,3], [1,2,3]))
