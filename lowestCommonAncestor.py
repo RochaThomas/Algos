c46b09a063278e0633bd16b8fc424df11c2c8fe4
RochaThomas <tomrocha99@gmail.com>
2022-08-30 19:33:21 -0700

    morning algos lowest common ancestor complete

--- lowestCommonAncestor.py
+++ lowestCommonAncestor.py
@@ -2,14 +2,23 @@
 # neetcode lowest common ancestor of a binary search tree
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
-        pass
+        curr = root
 
-    print(lowestCommonAncestor([6,2,8,0,4,7,9,None,None,3,5], 2, 8))
+        while curr:
+            if p.val > curr.val and q.val > curr.val:
+                curr = curr.right
+            elif p.val < curr.val and q.val < curr.val:
+                curr = curr.left
+            else:
+                return curr
+
+    print(lowestCommonAncestor(root = [6,2,8,0,4,7,9,None,None,3,5], p = 2, q = 8))
