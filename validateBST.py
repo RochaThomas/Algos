cc5549dcfdb88b75c76e531fec350e0167749dc3
RochaThomas <tomrocha99@gmail.com>
2022-09-02 23:39:05 -0700

    morning algos validate bianry search tree

---
+++ validateBST.py
@@ -0,0 +1,16 @@
+# morning algos
+# neetcode validate binary search tree
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root):
+        pass
+
+    print(isValidBST([2,1,3]))
+
+    
