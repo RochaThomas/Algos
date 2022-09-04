32694f10d22eff3a597e1a03b6ef70ea68cecb5a
RochaThomas <tomrocha99@gmail.com>
2022-09-04 14:50:53 -0700

    morning algos valid binary search tree complete

--- validateBST.py
+++ validateBST.py
@@ -7,10 +7,22 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+import collections
+
+
 class Solution:
     def isValidBST(self, root):
-        pass
+        
+        def valid(node, left, right):
+            if not node:
+                return True
+
+            if not (node.val < right and node.val > left):
+                return False
+
+            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
+
+        return valid(root, float("-inf"), float("inf"))
 
     print(isValidBST([2,1,3]))
 
-    
