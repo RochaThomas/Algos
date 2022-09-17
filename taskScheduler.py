4628b30371f2df4697263a5b3f1e55725440d674
RochaThomas <tomrocha99@gmail.com>
2022-09-16 20:39:40 -0700

    morning algos task scheduler

---
+++ taskScheduler.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Task Scheduler
+
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        pass
+
+    print(leastInterval(["A","A","A","B","B","B"], 2))
