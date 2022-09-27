604e51871e87513945a90405fef3f193ee3838a8
RochaThomas <tomrocha99@gmail.com>
2022-09-27 11:54:54 -0700

    morning algos palindrome partitioning

---
+++ palindromePartioning.py
@@ -0,0 +1,34 @@
+# morning algos
+# neetcode palindrome partitioning
+
+class Solution:
+    def partition(self, s):
+        res = []
+        part = []
+
+        def dfs(i):
+            if i >= len(s):
+                res.append(part[:])
+                return
+            
+            for j in range(i, len(s)):
+                if self.isPali(s, i, j):
+                    part.append(s[i : j + 1])
+                    dfs(j + 1)
+                    part.pop()
+
+
+        dfs(0)
+        return res
+    
+    def isPali(self, s, l, r):
+        while l < r:
+            if s[l] != s[r]:
+                return False
+            
+            l += 1
+            r -= 1
+        
+        return True
+
+    print(partition("aab"))
