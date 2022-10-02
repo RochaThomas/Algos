71c276fce8c4b2ab141319df3e43e7021f68760f
RochaThomas <tomrocha99@gmail.com>
2022-10-01 17:53:47 -0700

    morning algos clone graph

---
+++ cloneGraph.py
@@ -0,0 +1,16 @@
+# morning algos
+# neetcode Clone Graph
+
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node):
+        pass
+
+    print(cloneGraph([[2,4],[1,3],[2,4],[1,3]]))
