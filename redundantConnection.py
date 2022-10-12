4635d2d042cb3cbe072582dd85fadb382aa553f0
RochaThomas <tomrocha99@gmail.com>
2022-10-12 09:15:50 -0700

    morning algos rendundant connection

---
+++ redundantConnection.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Redundant Connection
+
+class Solution:
+    def findRedundantConnection(self, edges):
+        pass
+
+    print(findRedundantConnection([[1,2],[1,3],[2,3]]))
