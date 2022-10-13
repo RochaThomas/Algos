9decce433dee6494b59292d0a9851e83372572ef
RochaThomas <tomrocha99@gmail.com>
2022-10-13 11:35:33 -0700

    morning algos word ladder

---
+++ wordLadder.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Word Ladder
+
+class Solution:
+    def ladderLength(self, beginWord, endWord, wordList):
+        pass
+
+    print(ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
