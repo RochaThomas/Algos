57b927bd6d1b517424a09b48f5baf2f27c0dd11a
RochaThomas <tomrocha99@gmail.com>
2022-10-14 14:51:19 -0700

    morning algos reconstruct Itinerary

---
+++ reconstructItinerary.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Reconstruct Itinerary
+
+class Solution:
+    def findItinerary(self, tickets):
+        pass
+
+    print(findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
