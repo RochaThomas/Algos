dd54a55a4d29e6c82f674cfd3306b54e648a04f0
RochaThomas <tomrocha99@gmail.com>
2022-10-15 09:26:17 -0700

    morning algos min cost connect points

---
+++ minCostConnectPoints.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Min Cost to Connect All Points
+
+class Solution:
+    def minCostConnectPoints(self, points):
+        pass
+
+    print(minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]]))
