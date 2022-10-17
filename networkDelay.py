9f52f182b5e7c273604a146bf18d1f0d9ee7790c
RochaThomas <tomrocha99@gmail.com>
2022-10-17 13:05:44 -0700

    morning algos networkDelay

---
+++ networkDelay.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Network Delay Time
+
+class Solution:
+    def networkDelayTime(self, times, n, k):
+        pass
+
+    print(networkDelayTime([[2,1,1],[2,3,1],[3,4,1]], 4, 2))
