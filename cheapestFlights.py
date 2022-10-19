bbd84470337798baff0cd2f98042331d9c1f66ea
RochaThomas <tomrocha99@gmail.com>
2022-10-19 10:20:08 -0700

    morning algos cheapest flights with k stops

---
+++ cheapestFlights.py
@@ -0,0 +1,9 @@
+# morning algos
+# neetcode Cheapest Flights with K Stops
+
+class Solution:
+    def findCheapestPrice(self, n, flights, src, dst, k):
+        pass
+
+    print(findCheapestPrice( 4, [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], 0, 3, 1))
+    
