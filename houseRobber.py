4f4faa59bf350f27c8e00e978822953a9949c275
RochaThomas <tomrocha99@gmail.com>
2022-10-24 12:11:32 -0700

    morning algos house robber complete

--- houseRobber.py
+++ houseRobber.py
@@ -3,6 +3,12 @@
 
 class Solution:
     def rob(self, nums):
-        pass
+        rob1, rob2 = 0, 0
+
+        for n in nums:
+            temp = max(n + rob1, rob2)
+            rob1 = rob2
+            rob2 = temp
+        return rob2
 
     print(rob([1,2,3,1]))
