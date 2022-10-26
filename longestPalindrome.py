308d79df8e6c0bee7f39f0adfd16919e0ebf33f2
RochaThomas <tomrocha99@gmail.com>
2022-10-26 10:18:25 -0700

    morning algos longest palindromic substr complete

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -3,6 +3,28 @@
 
 class Solution:
     def longestPalindrome(self, s):
-        pass
+        res = ""
+        resLen = 0
 
+        for i in range(len(s)):
+            # check odd length first
+            l, r = i, i
+            while l >= 0 and r < len(s) and s[l] == s[r]: 
+                newLen = r - l + 1
+                if newLen > resLen:
+                    resLen = newLen
+                    res = s[l:r+1]
+                r += 1
+                l -= 1
+
+            # check even length
+            l, r = i, i + 1
+            while l >= 0 and r < len(s) and s[l] == s[r]: 
+                newLen = r - l + 1
+                if newLen > resLen:
+                    resLen = newLen
+                    res = s[l:r+1]
+                r += 1
+                l -= 1
+        return res
     print(longestPalindrome("babad"))
