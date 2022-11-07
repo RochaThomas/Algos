6d5bb4ca9c39979590762c1f4e659179b1a62312
RochaThomas <tomrocha99@gmail.com>
2022-11-07 09:20:42 -0800

    morning algos Longest Increasing Subseq complete

--- lengthOfLIS.py
+++ lengthOfLIS.py
@@ -3,6 +3,13 @@
 
 class Solution:
     def lengthOfLIS(self, nums):
-        pass
+        LIS = [1] * len(nums)
+
+        for i in range((len(nums) - 1), -1, -1):
+            for j in range((i + 1), len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+
+        return max(LIS)
 
     print(lengthOfLIS([10,9,2,5,3,7,101,18]))
