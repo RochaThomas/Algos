cbb802ac4b2d956568a1ab615f44126908a2b6a4
RochaThomas <tomrocha99@gmail.com>
2022-11-09 08:01:15 -0800

    morning algos unique paths complete

--- uniquePaths.py
+++ uniquePaths.py
@@ -3,6 +3,15 @@
 
 class Solution:
     def uniquePaths(self, m, n):
-        pass
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+
+        return row[0]
+
 
     print(uniquePaths(3, 7))
