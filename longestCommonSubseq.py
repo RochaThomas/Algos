9b2690392efbd5bc5b8d7f30155f5d9f54cf5cb5
RochaThomas <tomrocha99@gmail.com>
2022-11-20 07:44:52 -0800

    morning algos longest common subsequence

---
+++ longestCommonSubseq.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Longest Common Subsequence
+
+class Solution:
+    def longestCommonSubsequence(self, text1, text2):
+        pass
+
+    print(longestCommonSubsequence("abcde", "ace"))
