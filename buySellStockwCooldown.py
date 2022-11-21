73958a89f73ac827b0c6a3e7bd4aef87c36a1879
RochaThomas <tomrocha99@gmail.com>
2022-11-21 08:31:40 -0800

    morning algos buy & sell stock w cooldown complete

--- buySellStockwCooldown.py
+++ buySellStockwCooldown.py
@@ -3,6 +3,28 @@
 
 class Solution:
     def maxProfit(self, prices):
-        pass
+        # state: buying or selling?
+            # if buy i + 1
+            # if sell i + 2
+
+        # key = (i, buying) val = max_profit
+        dp = {}
+
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+            if (i, buying) in dp:
+                return dp[(i, buying)]
+            
+            cooldown = dfs(i + 1, buying)
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                dp[(i, buying)] = max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                dp[(i, buying)] = max(sell, cooldown)
+            return dp[(i, buying)]
+        
+        return dfs(0, True)
 
     print(maxProfit([1,2,3,0,2]))
