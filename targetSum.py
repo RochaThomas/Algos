543fe0a404480564214d9346dfd4a0108e4fb628
RochaThomas <tomrocha99@gmail.com>
2022-11-23 22:31:31 -0800

    morning algos target sum complete

--- targetSum.py
+++ targetSum.py
@@ -3,6 +3,18 @@
 
 class Solution:
     def findTargetSumWays(self, nums, target):
-        pass
+        dp = {}
+
+        def backtrack(i, total):
+            if i == len(nums):
+                return 1 if total == target else 0
+            if (i, total) in dp:
+                return dp[(i, total)]
+        
+            dp[(i, total)] = (backtrack(i + 1, total + nums[i]) +
+                            backtrack(i + 1, total - nums[i]))
+            
+            return dp[(i, total)]
+        return backtrack(0, 0)
 
     print(findTargetSumWays([1,1,1,1,1], 3))
