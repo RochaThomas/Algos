7eda127788ff886ef1466646a7bf456203e611a0
RochaThomas <tomrocha99@gmail.com>
2022-11-25 10:24:52 -0800

    morning algos longest increasing path in a matrix

---
+++ longestIncreasingPath.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Longest Increasing Path in a Matrix
+
+class Solution:
+    def longestIncreasingPath(self, matrix):
+        pass
+
+    print(longestIncreasingPath([[9,9,4],[6,6,8],[2,1,1]]))
