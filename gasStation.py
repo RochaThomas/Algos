b611757ea748fa31894d8e7ad6a31b3e50fceb90
RochaThomas <tomrocha99@gmail.com>
2022-12-12 11:26:00 -0800

    morning algos gas station complete

--- gasStation.py
+++ gasStation.py
@@ -3,6 +3,18 @@
 
 class Solution:
     def canCompleteCircuit(self, gas, cost):
-        pass
+        if sum(gas) < sum(cost):
+            return -1
+
+        total = 0
+        res = 0
+        for i in range(len(gas)):
+            total += (gas[i] - cost[i])
+
+            if total < 0:
+                total = 0
+                res = i + 1
+        
+        return res
 
     print(canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]))
