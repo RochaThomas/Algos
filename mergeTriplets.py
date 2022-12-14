5c1e7b6edf826030fea626f28587ed6e700793a2
RochaThomas <tomrocha99@gmail.com>
2022-12-14 10:07:45 -0800

    morning algos merge triplets

---
+++ mergeTriplets.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Merge Triplets to Form Target Triplet
+
+class Solution:
+    def mergeTriplets(self, triplets, target):
+        pass
+
+    print(mergeTriplets([[2,5,3],[1,8,4],[1,7,5]], [2,7,5]))
