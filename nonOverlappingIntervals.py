7a4d06e69c9cb2156bbcf90acd6e0cfa8b13ae61
RochaThomas <tomrocha99@gmail.com>
2022-12-24 11:04:28 -0800

    morning algos non overlapping intervals

---
+++ nonOverlappingIntervals.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Non-overlapping Intervals
+
+class Solution:
+    def eraseOverlapIntervals(self, intervals):
+        pass
+
+    print(eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]))
