a4299ab0a77bc821dbe2548f3788cee4b18a0caf
RochaThomas <tomrocha99@gmail.com>
2022-12-27 14:36:01 -0800

    morning algos min interval

---
+++ minInterval.py
@@ -0,0 +1,8 @@
+# morning algos
+# neetcode Minimum Interval to Include Each Query
+
+class Solution:
+    def minInterval(self, intervals, queries):
+        pass
+
+    print(minInterval([[1,4],[2,4],[3,6],[4,4]], [2,3,4,5]))
