efdff8108b6f023601b5a8b0a8981d9f062de685
RochaThomas <tomrocha99@gmail.com>
2022-12-29 12:10:38 -0800

    morning algos spiral matrix complete

--- spiralMatrix.py
+++ spiralMatrix.py
@@ -3,6 +3,32 @@
 
 class Solution:
     def spiralOrder(self, matrix):
-        pass
+        res = []
+        l, r = 0, len(matrix[0])
+        t, b = 0, len(matrix)
+
+        while l < r and t < b:
+            # get every i in top row
+            for i in range(l, r):
+                res.append(matrix[t][i])
+            t += 1
+            # get every i in right col
+            for i in range(t, b):
+                res.append(matrix[i][r - 1])
+            r -= 1
+
+            if not (l < r and t < b):
+                break
+
+            # get every i in bottom row
+            for i in range(r - 1, l - 1, -1):
+                res.append(matrix[b - 1][i])
+            b -= 1
+            # get every i in left col
+            for i in range(b - 1, t - 1, -1):
+                res.append(matrix[i][l])
+            l += 1
+
+        return res
 
     print(spiralOrder([[1,2,3],[4,5,6],[7,8,9]]))
