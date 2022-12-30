36f98b840f307c00c02bcc48d65542ae6adeb187
RochaThomas <tomrocha99@gmail.com>
2022-12-30 09:51:26 -0800

    morning algos set matrix zeroes

---
+++ setMatrixZeroes.py
@@ -0,0 +1,11 @@
+# morning algos
+# neetcode Set Matrix Zeroes
+
+class Solution:
+    def setZeroes(self, matrix):
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        pass
+
+    print(setZeroes([[1,1,1],[1,0,1],[1,1,1]]))
