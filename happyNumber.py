0cee63ec027c163f2274ac92152d697efdb7df7f
RochaThomas <tomrocha99@gmail.com>
2022-12-31 16:00:14 -0800

    morning algos happy number complete

--- happyNumber.py
+++ happyNumber.py
@@ -3,6 +3,26 @@
 
 class Solution:
     def isHappy(self, n):
-        pass
+        visit = set()
+
+        while n not in visit:
+            visit.add(n)
+            n = self.sumOfSquares(n)
+
+            if n == 1:
+                return True
+        
+        return False
+
+    def sumOfSquares(self, num):
+        output = 0
+
+        while num:
+            digit = num % 10
+            digit = digit ** 2
+            output += digit
+            num = num // 10
+        
+        return output
 
     print(isHappy(19))
