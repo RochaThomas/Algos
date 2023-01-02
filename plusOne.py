a6d1e5b5075db18ee2e83f871a1d188987bb5dd0
RochaThomas <tomrocha99@gmail.com>
2023-01-01 19:30:27 -0800

    morning algos plus one complete

--- plusOne.py
+++ plusOne.py
@@ -3,6 +3,22 @@
 
 class Solution:
     def plusOne(self, digits):
-        pass
+        digits = digits[::-1]
+        one, i = 1, 0
+
+        while one:
+            if i < len(digits):
+                if digits[i] == 9:
+                    digits[i] = 0
+                else:
+                    digits[i] += 1
+                    one = 0
+            else:
+                digits.append(1)
+                one = 0
+            i += 1
+
+        return digits[::-1]
+
 
     print(plusOne([1,2,3]))
