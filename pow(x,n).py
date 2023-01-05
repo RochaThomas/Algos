e52d71a927b3d5a80660b3db4c822b389b76f0cd
RochaThomas <tomrocha99@gmail.com>
2023-01-04 17:12:56 -0800

    morning algos pow(x, n) complete

--- pow(x,n).py
+++ pow(x,n).py
@@ -4,6 +4,14 @@
 
 class Solution:
     def myPow(self, x, n):
-        pass
+        def helper(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+
+            res = helper(x * x, n // 2)
+            return x * res if n % 2 else res
+        
+        res = helper(x, abs(n))
+        return res if n >= 0 else 1 / res
 
     print(myPow(2.00000, 10))
