66e35a44e7a3491afb478ffa89edf95ffa070883
RochaThomas <tomrocha99@gmail.com>
2023-01-11 11:00:19 -0800

    morning algos number of 1 bits complete

--- numberOf1Bits.py
+++ numberOf1Bits.py
@@ -1,8 +1,13 @@
 # morning algos
-# neetcode Number of1 Bits
+# neetcode Number of 1 Bits
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        pass
+        res = 0
+        while n: 
+            n &= (n - 1)
+            res += 1
+
+        return res
 
     # print(hammingWeight(n = 00000000000000000000000010000000))
