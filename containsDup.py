d209683aca60f09035feb10073dca52e7654a9c6
RochaThomas <tomrocha99@gmail.com>
2023-01-20 10:24:47 -0800

    morning algos contains duplicate practice

--- containsDup.py
+++ containsDup.py
@@ -1,26 +1,9 @@
 # morning algos
-# neet code 150 Arrays and Hashing #1
-# there is also a one line solution using set()
+# neet code Contains Duplicate
 
 
-# faster but more memory
-# def containsDuplicate(arr):
-#     map = {}
-#     for num in arr:
-#         if num in map:
-#             return True
-#         else:
-#             map[num] = 1
+class Solution:
+    def containsDuplicate(self, nums):
+        pass
 
-#     return False
-
-# slower but less memory
-def containsDuplicate(nums):
-    nums.sort()
-    for i in range(len(nums) - 1):
-        if nums[i] == nums[i+1]:
-            return True
-    return False
-
-
-print(containsDuplicate([1,2,3,4,5,6,6]))
+    print(containsDuplicate([1,2,3,1]))
