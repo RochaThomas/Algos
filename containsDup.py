218e145a42644a55fae19e6207158a0d5f078189
RochaThomas <tomrocha99@gmail.com>
2023-01-20 10:39:22 -0800

    morning algos contains duplicate practice complete

--- containsDup.py
+++ containsDup.py
@@ -4,6 +4,25 @@
 
 class Solution:
     def containsDuplicate(self, nums):
-        pass
+        # faster solution that sacrifices a little bit of space
+        # Time: O(n)
+        # Space: O(n)
+        n = set()
+
+        for num in nums:
+            if num in n: return True
+            n.add(num)
+        return False
+
+        # slower solution but doesn't sacrafice space
+        # Time: O(nlogn) because of sorting
+        # Space: O(1)
+        # nums.sort()
+        # for i in range(len(nums) - 1):
+        #     if nums[i] == nums[i + 1]:
+        #         return True
+        # return False
+
+
 
     print(containsDuplicate([1,2,3,1]))
