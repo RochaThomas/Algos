052e670e38325ecba883db31f9b5e0c983878928
RochaThomas <tomrocha99@gmail.com>
2023-01-25 08:30:22 -0800

    morning algos top k freq elements practice

--- topKFrequentElements.py
+++ topKFrequentElements.py
@@ -1,49 +1,8 @@
 # morning algos
-# top k frequent elements
+# neetcode Top K Frequent Elements
 # neet code 150 #5
 
-def topKFrequent(nums, k):
-    
-    output = []
-    num_dict = {}
-    
-    nums = sorted(nums)
-    count = 0
-    last_num = nums[0]
-
-    x = 0
-    
-    while x < len(nums):
-        if nums[x] == last_num:
-            count += 1
-        else:
-            num_dict[last_num] = count
-            last_num = nums[x]
-            count = 1
-
-        if x+1 >= len(nums):
-            num_dict[last_num] = count
-        x += 1
-
-    
-    freq_array = sorted(num_dict.values(), reverse=True)
-
-    i = 0
-    x = 0
-
-    while i < len(freq_array) and x <= k:
-        if i == 0:
-            for num in num_dict:
-                if num_dict[num] == freq_array[i]:
-                    output.append(num)
-                    x += 1
-        elif freq_array[i] != freq_array[i-1]:
-            for num in num_dict:
-                if num_dict[num] == freq_array[i]:
-                    output.append(num)
-                    x += 1
-        i += 1
-
-    return output
-
-print(topKFrequent([1], 1))
+class Solution:
+    def topKFrequent(self, nums, k):
+        pass
+    print(topKFrequent([1], 1))
