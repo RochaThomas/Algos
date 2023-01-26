31d6d3833be3638afbc6399f34ca23a7544496f3
RochaThomas <tomrocha99@gmail.com>
2023-01-26 10:05:43 -0800

    morning algos prod of array except self practice

--- productOfArray.py
+++ productOfArray.py
@@ -1,37 +1,7 @@
 
-# def productExceptSelf(nums):
+# morning algos
+# neetcode Product of Array Except Self
 
-#     # works but is too slow and too much mem...
-#     output = []
-#     original_length = len(nums)
-
-#     nums += nums
-
-#     for i in range(original_length):
-#         prod = 1
-#         for x in range(i + 1, original_length + i):
-#             prod *= nums[x]
-#         output.append(prod)
-
-#     return output
-
-# most optimal solution
-def productExceptSelf(nums):
-    output = [1] * len(nums)
-    
-    prefix = 1
-
-    for i in range(len(nums)):
-        output[i] = prefix
-        prefix *= nums[i]
-
-    print(output)
-    postfix = 1
-
-    for i in range(len(nums) - 1, -1, -1):
-        output[i] *= postfix
-        postfix *= nums[i]
-
-    return output
-
-print(productExceptSelf([1,2,3,4]))
+class Solution:
+    def productExceptSelf(self, nums):
+        pass
