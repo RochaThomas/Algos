c32d8cd04f8c9b36d327dc4a2c2250429db1a379
RochaThomas <tomrocha99@gmail.com>
2023-02-02 09:35:58 -0800

    morning algos longest conseq seq prac

--- longestConsecutiveSequence.py
+++ longestConsecutiveSequence.py
@@ -1,20 +1,10 @@
 # morning algos
 # neetcode longest consecutive sequence
 
-# needed help to figure this solution out
-def longestConsecutive(nums):
-    nums_set = set(nums)
-    longest = 0
-
-    for num in nums:
-        if (num - 1) not in nums_set:
-            length = 0
-            while (num + length) in nums_set:
-                length += 1
-            if length > longest:
-                longest = length
-    return longest
+class Solution:
+    def longestConsecutive(self, nums):
+        pass
 
 
 
-print(longestConsecutive([100,4,200,1,3,2]))
+    print(longestConsecutive([100,4,200,1,3,2]))
