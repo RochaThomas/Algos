fffa9e90adb6c4b4a19330eb92026dd605c76292
RochaThomas <tomrocha99@gmail.com>
2023-02-09 13:07:36 -0800

    ctci ch5

---
+++ ch5BitManipulation.py
@@ -0,0 +1,10 @@
+# cracking the coding interview 
+# chapter 5 bit manipulation
+
+# interview questions
+
+# 5.1 insertion
+# given 2 32-bit numbers, m and n, and two bit positions, i and j. write a function that inserts m into n
+# such that m starts at j and ends at i
+def insertion(m, n, i, j):
+    pass
