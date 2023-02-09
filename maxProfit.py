5cd65bef421475ec87d496e079925103d26fbaf8
RochaThomas <tomrocha99@gmail.com>
2023-02-09 07:21:33 -0800

    morning algos buy and sell stocks prac

--- maxProfit.py
+++ maxProfit.py
@@ -1,36 +1,8 @@
 # morning algos
 # neetcode best time to buy and sell stock aka max profit
 
-# this solution works
-# def maxProfit(prices):
-#     max = 0
-#     low = prices[0]
+class Solution:
+    def maxProfit(self, prices):
+        pass
 
-#     for i in range(len(prices)):
-#         if prices[i] < low:
-#             low = prices[i]
-#         else:
-#             dif = prices[i] - low
-#             if dif > max:
-#                 max = dif
-            
-#     return max
-
-# solution using pointers but first solution is better
-def maxProfit(prices):
-    l, r = 0, 1
-    maxP = 0
-
-    while r < len(prices):
-        if prices[l] < prices[r]:
-            profit = prices[r] - prices[l]
-            maxP = max(profit, maxP)
-        else:
-            l = r
-        r += 1
-    
-    return maxP
-
-
-
-print(maxProfit([7,1,5,3,6,4]))
+    print(maxProfit([7,1,5,3,6,4]))
