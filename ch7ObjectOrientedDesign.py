edc797e6c916d7cedb6c752364a82f8319fb39b6
RochaThomas <tomrocha99@gmail.com>
2023-02-16 10:21:13 -0800

    ctci ch7 7.1 complete

--- ch7ObjectOrientedDesign.py
+++ ch7ObjectOrientedDesign.py
@@ -8,5 +8,35 @@
 design the data structures of a generic deck of cards. explain how you would subclass the data structure to implement
 blackjack
 
+for a 52 deck of cards
+
+suit class
+    takes in value and records a suit based on value
+
+deck class
+    has an array of 52 cards
+    keeps track of an index for the place in the deck
+    methods for shuffling and getting the number of remaining cards in the deck
+
+card class
+    has a value 1-13
+    has a suit
+    has a boolean to indicate whether or not it is available
+
+hand class
+    has an array of cards
+    has a score 
+    methods for adding and removing cards
+"""
+
+"""
+7.2 call center
+imagine you have a class center with 3 levels of employees, respondent, manager, and director
+incoming calls are directed to a free responder
+that call can be redirected to manager
+if the manager isn't free then it goes to the director
+design classes and data structures for this problem
+implement a method dispatchCall() which assigns a call to the first available employee
+
 
 """
