f0de96b9fae11d1a17d7d4fd82cb7d1312d6282e
RochaThomas <tomrocha99@gmail.com>
2023-02-16 10:03:34 -0800

    ctci ch7

---
+++ ch7ObjectOrientedDesign.py
@@ -0,0 +1,12 @@
+# cracking the coding interview
+# chapter 7 object oriented design
+
+# interview questions
+
+"""
+7.1 deck of cards
+design the data structures of a generic deck of cards. explain how you would subclass the data structure to implement
+blackjack
+
+
+"""
