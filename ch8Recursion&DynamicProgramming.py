bb5e3a574e24dabeb212145db321f2c7fd8ee281
RochaThomas <tomrocha99@gmail.com>
2023-02-21 11:00:20 -0800

    ctci ch8

---
+++ ch8Recursion&DynamicProgramming.py
@@ -0,0 +1,9 @@
+# cracking the coding interview
+# ch 8 recursion and dynamic programming
+
+# interview questions
+
+# 8.1 triple step
+# a child is running up a stair case with n steps and can take either 1, 2, or 3 steps at a time
+# implement a method to count how many possible ways the child can run up the steps
+
