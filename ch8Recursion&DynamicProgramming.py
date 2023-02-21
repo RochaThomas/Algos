1448f196dc8937d2381da90084ffa208af9fc6a3
RochaThomas <tomrocha99@gmail.com>
2023-02-21 12:28:23 -0800

    ctci ch8 8.2 complete

--- ch8Recursion&DynamicProgramming.py
+++ ch8Recursion&DynamicProgramming.py
@@ -19,12 +19,31 @@ def triple_step(n, memo = {}):
 
     return memo[n]
 
-
-print(triple_step(4))
-
 # 8.2 robot in a grid
 # image a robot in a grid that can only move right and now
-# given a grid with certain points that are off limits, design an algo that will find a path from the top left to the bottom right
+# given a grid with certain points that are off limits, design an algo that will find if a path from the top left to the bottom right
+# exists
 
-def robot_in_a_grid(off_limits):
-    pass
+def robot_in_a_grid(maze):
+    if maze == None or len(maze) == 0: return None
+
+    def getPath(maze, r, c, path, failed = {}):
+        # if out of bounds or off limits (meaning maze[r][c] is false) or failed spot (meaning we tried and there is no path) return false
+        if r < 0 or c < 0 or not maze[r][c] or (r, c) in failed:
+            return False
+        # if there is a path from the start to the current position then add position to path
+        if (r == 0 and c == 0) or robot_in_a_grid(maze, r, c - 1, path, failed) or robot_in_a_grid(maze, r - 1, c, path, failed):
+            path.add((r, c))
+            return True
+        # if there is no path then the position must fail so add position to failed and return False
+        failed.add((r, c))
+        return False
+        
+    path = []
+    failed = {}
+    # if there is a path return the path if not return None
+    if (getPath(maze, len(maze) - 1, len(maze[0] - 1), path, failed)):
+        return path
+
+    return None
+    
