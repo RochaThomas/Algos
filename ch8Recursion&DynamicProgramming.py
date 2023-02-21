85f86ca8ae2d4bcc9b04da95d6de6438cf40f90e
RochaThomas <tomrocha99@gmail.com>
2023-02-21 11:57:12 -0800

    ctci ch8 8.1 complete

--- ch8Recursion&DynamicProgramming.py
+++ ch8Recursion&DynamicProgramming.py
@@ -7,3 +7,24 @@
 # a child is running up a stair case with n steps and can take either 1, 2, or 3 steps at a time
 # implement a method to count how many possible ways the child can run up the steps
 
+def triple_step(n, memo = {}):
+    """
+    paths from a step to the end are step + 1, step + 2, or step + 3
+    """
+    if n < 0 : return 0
+    elif n == 0: return 1
+    elif n in memo: return memo[n]
+    else: 
+        memo[n] = triple_step(n - 1, memo) + triple_step(n - 2, memo) + triple_step(n - 3, memo)
+
+    return memo[n]
+
+
+print(triple_step(4))
+
+# 8.2 robot in a grid
+# image a robot in a grid that can only move right and now
+# given a grid with certain points that are off limits, design an algo that will find a path from the top left to the bottom right
+
+def robot_in_a_grid(off_limits):
+    pass
