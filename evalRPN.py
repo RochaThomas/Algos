4fbcd5f4a857fc05815f090072ebf59b4263b3a2
RochaThomas <tomrocha99@gmail.com>
2023-02-22 09:33:58 -0800

    morning algos evalRPN prac complete

--- evalRPN.py
+++ evalRPN.py
@@ -1,8 +1,24 @@
 # morning algos
 # neetcode evaluate reverse polish notation
 
-class Solution:
-    def evalRPN(self, tokens):
-        pass
 
-    print(evalRPN(["2","1","+","3","*"]))
+def evalRPN(tokens):
+    nums = []
+    for c in tokens:
+        if c == "+":
+            nums.append(nums.pop() + nums.pop())
+        elif c == "-":
+            n1, n2 = nums.pop(), nums.pop()
+            nums.append(n2 - n1)
+        elif c == "*":
+            nums.append(nums.pop() * nums.pop())
+        elif c == "/":
+            n1, n2 = nums.pop(), nums.pop()
+            nums.append(int(n2 / n1))
+        else:
+            nums.append(int(c))
+
+    return nums[0]
+
+
+print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
