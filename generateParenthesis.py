1d82cc54b6307f2501c84545b58077840e7920c0
RochaThomas <tomrocha99@gmail.com>
2023-02-23 09:29:14 -0800

    morning algos generate parenthesis prac complete

--- generateParenthesis.py
+++ generateParenthesis.py
@@ -3,6 +3,31 @@
 
 class Solution:
     def generateParenthesis(self, n):
-        pass
+        stack = []
+        res = []
+        def backtrack(openN, closedN):
+            # base case when the stack has the correct order and number of parenthesis
+            # make the stack into a string and append to result
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            
+            # only add open paren when number of open is less than n
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                # pop because we are only using 1 stack so we pop off the paren we just added
+                # so that we can make more combinations of parenthesis
+                stack.pop()
+            
+            # only add close paren when num of closed is less than open
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        
+        backtrack(0, 0)
+        return res
+
 
     print(generateParenthesis(3))
