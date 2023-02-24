a4b03ef268a913dc7e37a95f217a3674606bec8a
RochaThomas <tomrocha99@gmail.com>
2023-02-24 09:44:25 -0800

    morning algos daily temps prac

--- dailyTemperatures.py
+++ dailyTemperatures.py
@@ -1,22 +1,8 @@
 # morning algos
 # neetcode daily temperatures
 
-# neetcode solution... review video
-# solution looks confusing but video explains it well
-# go over enumerate functions
-# i = index, t = temperature
+class Solution:
+    def dailyTemperatures(self, temperatures):
+        pass
 
-def dailyTemperatures(temperatures):
-    res = [0] * len(temperatures)
-    # stack pairs: [temp, index]
-    stack = []
-
-    for i, t in enumerate(temperatures):
-        while stack and t > stack[-1][0]:
-            stackT, stackInd = stack.pop()
-            res[stackInd] = (i - stackInd)
-        stack.append([t, i])
-
-    return res
-
-print(dailyTemperatures([73,74,75,71,69,72,76,73]))
+    print(dailyTemperatures([73,74,75,71,69,72,7,6,73]))
