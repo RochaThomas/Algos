cd692dd4914f25bf0c332cf06d047f7e428387c5
RochaThomas <tomrocha99@gmail.com>
2023-03-01 09:40:25 -0800

    morning algos binary search prac

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,18 +1,8 @@
 # morning algos
 # neet code binary search
 
-def search(nums, target):
-    l, r = 0, len(nums) - 1
-    
-    while l <= r:
-        m  = (l + r) // 2
-        if nums[m] > target:
-            r = m - 1
-        elif nums[m] < target:
-            l = m + 1
-        else:
-            return m
+class Solution:
+    def search(self, nums, target):
+        pass
 
-    return  -1
-
-print(search([5], 5))
+    print(search([5], 5))
