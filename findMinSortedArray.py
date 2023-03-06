671e446ea2b350fa8fd928b1663f55eab4b6e835
RochaThomas <tomrocha99@gmail.com>
2023-03-06 09:56:42 -0800

    morning algos find min in rot sort array prac

--- findMinSortedArray.py
+++ findMinSortedArray.py
@@ -1,23 +1,8 @@
 # morning algos
 # neetcode find min in rotated sorted array
 
-def findMin(nums):
-    result = nums[0]
-    l, r = 0, len(nums) - 1
+class Solution:
+    def findMin(self, nums):
+        pass
 
-    while l <= r:
-        if nums[l] < nums[r]:
-            result = min(result, nums[l])
-            break
-
-        m = (l + r) // 2
-        result = min(result, nums[m])
-
-        if nums[m] >= nums[l]:
-            l = m + 1
-        else:
-            r = m - 1
-
-    return result
-
-print(findMin([3,4,5,1,2]))
+    print(findMin([3,4,5,1,2]))
