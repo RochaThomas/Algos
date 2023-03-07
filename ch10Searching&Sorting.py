9c4cb55df84811b170717ab237a2db7f9d311243
RochaThomas <tomrocha99@gmail.com>
2023-03-07 11:35:04 -0800

    ctci ch10 10.1 complete

--- ch10Searching&Sorting.py
+++ ch10Searching&Sorting.py
@@ -3,4 +3,7 @@
 
 # interview questions
 
-# 10.1 
+# 10.1 sorted merge
+# merge two sorted arrays where array a has a long enough buffer at the end to fit array b
+# start at the last number of each array, compare and add to end of array a
+
