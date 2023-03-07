13a6428f3aedd0bb6faedf1e4bc0e34342c31c42
RochaThomas <tomrocha99@gmail.com>
2023-03-07 11:36:51 -0800

    ctci ch10 10.2 complete

--- ch10Searching&Sorting.py
+++ ch10Searching&Sorting.py
@@ -7,3 +7,8 @@
 # merge two sorted arrays where array a has a long enough buffer at the end to fit array b
 # start at the last number of each array, compare and add to end of array a
 
+# 10.2 group anagram
+# sort a list of string so that all anagrams of each other are next to one another
+# make a hashmap where key is a sorted string and value is an array of strings, add string to array if it matches
+# key when sorted, return hashmap values
+
