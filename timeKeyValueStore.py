8f75382f74cf9a9eebc61439b6824368634cf636
RochaThomas <tomrocha99@gmail.com>
2023-03-08 10:11:44 -0800

    morning algos timeKeyValStore prac complete

--- timeKeyValueStore.py
+++ timeKeyValueStore.py
@@ -4,12 +4,26 @@
 class TimeMap:
 
     def __init__(self):
-        
+        self.map = {}
 
-    def set(self, key: str, value: str, timestamp: int) -> None:
-        
+    def set(self, key, value, timestamp):
+        if key in self.map:
+            self.map[key].append([value, timestamp])
+        else:
+            self.map[key] = [[value, timestamp]]
 
-    def get(self, key: str, timestamp: int) -> str:
+    def get(self, key, timestamp):
+        res = ""
+        if key in self.map:
+            l, r = 0, len(self.map[key]) - 1
+            while l <= r:
+                m = (l + r) // 2
+                if self.map[key][m][1] <= timestamp:
+                    res = self.map[key][m][0]
+                    l = m + 1
+                else:
+                    r = m - 1
+        return res
         
 
 
