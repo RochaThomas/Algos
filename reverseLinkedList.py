6522b9abfdfb24b7218f5352321568089e1683c0
RochaThomas <tomrocha99@gmail.com>
2023-03-10 09:10:19 -0800

    morning algos reverse linked list prac

--- reverseLinkedList.py
+++ reverseLinkedList.py
@@ -6,31 +6,8 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-
 class Solution:
-    def reverseList(head):
-        prev = None
-        runner = head
+    def reverseList(self, head):
+        pass
 
-        while runner:
-            temp = runner.next
-            runner.next = prev
-            prev = runner
-            runner = temp
-
-            if runner == None:
-                head = prev
-        return head
-
-# # recursive solution
-# class Solution:
-#     def reverseList(self, head):
-#         if not head:
-#             return None
-        
-#         newHead = head
-#         if head.next:
-#             newHead = self.reverseList(head.next)
-#             head.next.next = head
-#         head.next = None
-#         return newHead
+    print(reverseList([1,2,3,4,5]))
