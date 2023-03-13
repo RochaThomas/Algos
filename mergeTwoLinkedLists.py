7f5080c5866fa548dcd4c43018af9fcc0730fabc
RochaThomas <tomrocha99@gmail.com>
2023-03-13 08:19:02 -0700

    morning algos merge two sorted linked lists prac

--- mergeTwoLinkedLists.py
+++ mergeTwoLinkedLists.py
@@ -1,30 +1,13 @@
 # morning algos
 # neetcode merge two linked lists
 
-# neetcode solution
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1, list2):
+        pass
 
-def mergeTwoLists(list1, list2):
-    dummy = ListNode()
-    tail = dummy
-
-    while list1 and list2:
-        if list1.val < list2.val:
-            tail.next = list1
-            list1 = list1.next
-        else:
-            tail.next = list2
-            list2 = list2.next
-        tail = tail.next
-
-    if list1:
-        tail.next = list1
-    elif list2:
-        tail.next = list2
-
-    return dummy.next
-
-print(mergeTwoLists([1,2,4], [1,3,4]))
+    print(mergeTwoLists([1,2,4], [1,3,4]))
