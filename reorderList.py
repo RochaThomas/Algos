f34ef0e1557fd33968d61c8f55f6fbf82884a02d
RochaThomas <tomrocha99@gmail.com>
2023-03-14 09:03:10 -0700

    morning algos reorder list prac complete

--- reorderList.py
+++ reorderList.py
@@ -9,9 +9,36 @@
 class Solution:
     def reorderList(self, head):
         """
-        Do not return anything, modify head in-place instead.
+        get half, reverse second half of list, merge lists
         """
-        pass
+
+        slow = head
+        fast = head.next
+
+        # get to half
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        # reverse second half
+        second = slow.next
+        prev = slow.next = None
+
+        while second:
+            temp = second.next
+            second.next  = prev
+            prev = second
+            second = temp
+
+        # merge
+        first = head
+        second = prev
+        while second:
+            temp1, temp2 = first.next, second.next
+            first.next = second
+            second.next = temp1
+            first, second = temp1, temp2
+
 
 
     print(reorderList([1,2,3,4]))
