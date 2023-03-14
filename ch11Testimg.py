f3735407a19b54c6b901b7e1ac4825d1c56ffc80
RochaThomas <tomrocha99@gmail.com>
2023-03-14 10:53:00 -0700

    ctci ch11 11.1 complete

--- ch11Testimg.py
+++ ch11Testimg.py
@@ -4,3 +4,14 @@
 # interview questions
 
 # 11.1 mistake
+# find the mistake in the following code
+"""
+unsigned int i;
+for (i = 100; i >= 0; --i)
+    printf("%d\n", i);
+
+one mistake is you are using an unsigned int which is greater than or equal to 0 by definition
+so it will never be less than 0 resulting in an infinite loop
+second mistake is that when printing unsigned ints we need to use %u instead of %d
+"""
+
