3c4c20d183167b91088d6ad67df43169ebff51db
RochaThomas <tomrocha99@gmail.com>
2023-03-15 09:37:17 -0700

    morning algos removeNthFromEnd prac complete

--- removeNthFromEnd.py
+++ removeNthFromEnd.py
@@ -8,9 +8,24 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
 class Solution:
     def removeNthFromEnd(self, head, n):
-        pass
+        dummy = ListNode(0, head)
+        l, r = dummy, head
+
+        while n > 0 and r:
+            r = r.next
+            n -= 1
+
+        while r:
+            l = l.next
+            r = r.next
+
+        l.next = l.next.next
+
+        return head
+
 
     print(removeNthFromEnd([1,2,3,4,5], 2))
         
