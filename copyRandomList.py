1b8301bbfaf0d946fdbbbf4401e3a2e175066f7a
RochaThomas <tomrocha99@gmail.com>
2023-03-16 09:33:53 -0700

    morning algos copyRandomList prac complete

--- copyRandomList.py
+++ copyRandomList.py
@@ -1,17 +1,31 @@
 # morning algos
 # neetcode Copy List with Random Pointer
 
-"""
+
 # Definition for a Node.
 class Node:
     def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
         self.val = int(x)
         self.next = next
         self.random = random
-"""
 
 class Solution:
     def copyRandomList(self, head):
-        pass
+        oldToCopy = {None: None}
+        curr = head
+        
+        while curr:
+            copy = Node(curr.val)
+            oldToCopy[curr] = copy
+            curr = curr.next
+
+        curr = head
+        while curr:
+            copy = oldToCopy[curr]
+            copy.next = oldToCopy[curr.next]
+            copy.random = oldToCopy[curr.random]
+            curr = curr.next
+
+        return oldToCopy[head]
 
     print(copyRandomList([[7,None],[13,0],[11,4],[10,2],[1,0]]))
