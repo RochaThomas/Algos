ceba611fb5d0ce4e74181bcf26470e32729ce105
RochaThomas <tomrocha99@gmail.com>
2023-03-20 10:49:22 -0700

    morning algos linkListCycle prac complete

--- linkedListCycle.py
+++ linkedListCycle.py
@@ -9,7 +9,18 @@ class ListNode:
 
 class Solution:
     def hasCycle(self, head):
-        pass
+        """
+        two solutions
+        first is make a hashmap checking to see if the current node is already in the map
+        second is to use a runner moving twice as fast
+        """
+        curr = runner = head
+        while runner and runner.next:
+            curr = curr.next
+            runner = runner.next.next
+            if curr == runner:
+                return True
+        return False
 
 
     print(hasCycle([3,2,0,-4]))
