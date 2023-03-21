ab296d5f81f04c37ca61360d9cd4679ccd21a780
RochaThomas <tomrocha99@gmail.com>
2023-03-21 09:49:02 -0700

    morning algos findDuplicateNumber prac complete

--- findDuplicate.py
+++ findDuplicate.py
@@ -3,6 +3,20 @@
 
 class Solution:
     def findDuplicate(self, nums):
-        pass
+        slow = fast = 0
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            if slow == fast:
+                break
+
+        slow = 0
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
 
     print(findDuplicate([1,3,4,2,2]))
