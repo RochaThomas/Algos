481a807be7b0122b86c0504339ed85d2919dfe3a
RochaThomas <tomrocha99@gmail.com>
2023-03-21 09:29:10 -0700

    morning algos find duplicate number prac

--- findDuplicate.py
+++ findDuplicate.py
@@ -1,23 +1,8 @@
 # morning algos
 # neetcode find the duplicate
 
-# tricky problem, pretty much just memorization for this one
-def findDuplicate(nums):
-    s, f = 0, 0
+class Solution:
+    def findDuplicate(self, nums):
+        pass
 
-    while True:
-        s = nums[s]
-        f = nums[nums[f]]
-
-        if s == f:
-            break
-
-    s2 = 0
-    while True:
-        s = nums[s]
-        s2 = nums[s2]
-
-        if s == s2:
-            return s
-
-print(findDuplicate([1,3,4,2,2]))
+    print(findDuplicate([1,3,4,2,2]))
