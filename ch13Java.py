ddc11a81dc42e91bfc6a55270c534f5ac3cbd655
RochaThomas <tomrocha99@gmail.com>
2023-03-27 11:17:27 -0700

    ctci ch13 13.8 complete

--- ch13Java.py
+++ ch13Java.py
@@ -120,3 +120,18 @@ int getPopulation(List<Country> countries, String continent) {
 }
 """
 
+# 13.8 lambda random
+# using lambda expressions, write a function List<Integer> getRandomSubset(List<Integer> list) that returns a
+# random subset of arbitrary size. All subsets (including the empty set) should be equally likely
+"""
+Random random = new Random();
+Predicate<Object> flipCoin = o -> {
+    return random.nextBoolean();
+}
+
+List<Integer> getRandomSubset(List<Integer> list) {
+    List<Integer> subset = list.stream().filter(flipCoin).
+        collect(Collectors.toList());
+    return subset;
+}
+"""
