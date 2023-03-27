f317c098cb5a59f7868f8c96775124bc3857dbf2
RochaThomas <tomrocha99@gmail.com>
2023-03-27 10:23:13 -0700

    ctci ch13 13.3 complete

--- ch13Java.py
+++ ch13Java.py
@@ -24,3 +24,25 @@ the finally block only doesn't execute if the virtual machine exits before the f
 or if the thread that is running the try catch block gets killed
 """
 
+# 13.3 final, etc
+# what is the difference between final, finally, and finalize?
+"""
+genrally
+final - controls whether a method, variable, or class is changeable
+finally - is used in a try/catch block to make sure a piece of code always runs
+finalize - is a method called by the garbage collector once it determines no more references exist
+
+specifics
+final
+    when applied to primitive var, value can not change
+    when applied to var reference, the reference cannot change to point to another obj on the heap
+    when applied to a method, the method cannot be overridden
+    when applied to a class, the class cannot be subclassed
+finally
+    this block executes even if an exeption is thrown
+    its usually used to clean up code
+finalize
+    this is called just before the automatic garbage collector actually destroys an obj
+    this method can be overridden if someone wishes to define custom behavior for garbage collection
+"""
+
