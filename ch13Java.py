4748da35c0f2a6116a499b3efe3294bb16e83c29
RochaThomas <tomrocha99@gmail.com>
2023-03-27 10:12:51 -0700

    ctci ch13 13.1

--- ch13Java.py
+++ ch13Java.py
@@ -6,5 +6,12 @@
 # 13.1 private constructor
 # in terms of inheritance what is the purpose of keeping a constructor private?
 """
+making a constructor private means you can only access the constructor if you can also access it's private methods
+the only things that can access these private methods are the class itself, the inner classes of this class, and the other
+inner classes of this classes parent class
+meaning if A and B are inner classes of Q and C is an inner class of A then ABC can access the private constructor
 
+This demonstrate inheritance because a subclass calls its parent's constructor. Meaning the A class can be inherited by its own
+subclasses or by its parent's other subclasses
 """
+
