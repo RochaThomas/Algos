93d35554137890eb7322544b64d162a01b1f56b7
RochaThomas <tomrocha99@gmail.com>
2023-03-27 10:05:42 -0700

    ctci ch13

---
+++ ch13Java.py
@@ -0,0 +1,10 @@
+# cracking the coding interview
+# chapter 13 java
+
+# interview questions
+
+# 13.1 private constructor
+# in terms of inheritance what is the purpose of keeping a constructor private?
+"""
+
+"""
