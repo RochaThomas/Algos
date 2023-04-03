19632023cb60e1236ca766c39142fc9358ed9c2b
RochaThomas <tomrocha99@gmail.com>
2023-04-03 08:54:13 -0700

    ctci ch14

---
+++ ch14Databases.py
@@ -0,0 +1,7 @@
+# cracking the coding interview
+# chapter 14 databases
+
+# interview questions
+
+# 14.1 multiple apartments
+# write a sql query to get a list of tenants who are renting more than one apartment
