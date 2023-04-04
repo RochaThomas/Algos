e6a1f4bbdf94c5406224879827e396447052e3be
RochaThomas <tomrocha99@gmail.com>
2023-04-04 11:17:44 -0700

    ctci ch14 14.1 complete

--- ch14Databases.py
+++ ch14Databases.py
@@ -5,3 +5,16 @@
 
 # 14.1 multiple apartments
 # write a sql query to get a list of tenants who are renting more than one apartment
+"""
+SELECT TenantName
+FROM Tenants
+INNER JOIN
+    (SELECT TenantID FROM AptTenants GROUP BY TenantID HAVING count(*) > 1) C
+ON Tenants.TenantID = C.TenantID
+"""
+
+# 14.2 open requests
+# write a sql query to get a list of all buildings and the number of open requests (requests in which status equals open)
+"""
+
+"""
