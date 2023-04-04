424a02daae63c19ae22d3390228b3fe097dbaddd
RochaThomas <tomrocha99@gmail.com>
2023-04-04 10:19:14 -0700

    morning algos subTreeOfAnother prac complete

--- subtreeOfAnother.py
+++ subtreeOfAnother.py
@@ -9,6 +9,19 @@ class TreeNode:
         self.right = right
 class Solution:
     def isSubtree(self, root, subRoot):
-        pass
+        if not subRoot: return True
+        if not root: return False
+        
+        if self.checkTree(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+
+    def checkTree (self, node, subRoot):
+        if not node and not subRoot: return True
+        if node and subRoot and node.val == subRoot.val:
+            return self.checkTree(node.left, subRoot.left) and self.checkTree(node.right, subRoot.right)
+        return False
+
 
     print(isSubtree([3,4,5,1,2], [4,1,2]))
