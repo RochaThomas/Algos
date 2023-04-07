7475ec40a8c26947c1b77b69b6ff4a7c77ab615c
RochaThomas <tomrocha99@gmail.com>
2023-04-07 07:25:05 -0700

    morning algos rightSideView prac

--- rightSideView.py
+++ rightSideView.py
@@ -2,41 +2,13 @@
 # neetcode binary tree right side view
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
-
-import collections
-
-
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def rightSideView(self, root):
-        output = []
-
-        if not root:
-            return output
-
-        q = collections.deque()
-        q.append(root)
-        
-        while q:
-            qLen = len(q)
-            level = []
-            for i in range(qLen):
-                node = q.popleft()
-
-                if node:
-                    level.append(node.val)
-                    q.append(node.left)
-                    q.append(node.right)
-
-            if level:
-                output.append(level[-1])
-        
-        return output
-
+        pass
 
     print(rightSideView([1,2,3,None,5,None,4]))
