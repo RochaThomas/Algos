0f1799c759a173ccd07128dd94b9ac2fd60c2b4e
RochaThomas <tomrocha99@gmail.com>
2023-04-11 07:31:58 -0700

    morning algos validate BST prac

--- validateBST.py
+++ validateBST.py
@@ -2,27 +2,14 @@
 # neetcode validate binary search tree
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-import collections
-
-
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def isValidBST(self, root):
-        
-        def valid(node, left, right):
-            if not node:
-                return True
-
-            if not (node.val < right and node.val > left):
-                return False
-
-            return (valid(node.left, left, node.val) and valid(node.right, node.val, right))
-
-        return valid(root, float("-inf"), float("inf"))
+        pass
 
     print(isValidBST([2,1,3]))
 
