44bf6d78f3db36f7ae522043340bfdfca6906ae3
RochaThomas <tomrocha99@gmail.com>
2023-04-12 07:13:48 -0700

    morning algos kthSmallest prac

--- kthSmallest.py
+++ kthSmallest.py
@@ -2,28 +2,14 @@
 # neetcode Kth Smallest Element in a BST
 
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def kthSmallest(self, root, k):
-        # n is the number of nodes that we have visited
-        n = 0
-        stack = []
-        curr = root
-
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-
-            curr = stack.pop()
-            n += 1
-            if n == k:
-                return curr.val
-            curr = curr.right
+        pass
 
 
     print(kthSmallest([3,1,4,None,2], 1))
