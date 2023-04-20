0be48db7c3467e8432da9f667aacbee3432de21e
RochaThomas <tomrocha99@gmail.com>
2023-04-20 09:40:02 -0700

    morning algos wordSearchII

--- wordSearchII.py
+++ wordSearchII.py
@@ -1,56 +1,8 @@
 # morning algos
 # neetcode Word Search 2
 
-# this solution works but its slowish
-# to make it faster and to pass the leetcode test cases you need to prune words after they are found
-# if you find a word then remove it from the Trie\
-# this then reduces the amount of searching that has to be done (the number of dfs calls you have to make) 
-
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.endOfWord = False
-
-    def addWord(self, word):
-        curr = self
-
-        for c in word:
-            if c not in curr.children:
-                curr.children[c] = TrieNode()
-            curr = curr.children[c]
-        
-        curr.endOfWord = True
-
 class Solution:
     def findWords(self, board, words):
-        root = TrieNode()
-
-        for word in words:
-            root.addWord(word)
-
-        ROWS, COLS = len(board), len(board[0])
-        res, visit = set(), set()
-
-        def dfs(r, c, node, word):
-            if (r < 0 or c < 0 or r == ROWS or c == COLS or (r,c) in visit or board[r][c] not in node.children):
-                return
-            visit.add((r,c))
-
-            node = node.children[board[r][c]]
-            word += board[r][c]
-            if node.endOfWord:
-                res.add(word)
-
-            dfs(r + 1, c, node, word)
-            dfs(r, c + 1, node, word)
-            dfs(r - 1, c, node, word)
-            dfs(r, c - 1, node, word)
-            visit.remove((r,c))
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                dfs(r, c, root, "")
-
-        return list(res)
+        pass
 
     print(findWords(board = [["o","a","a","n"],["e","t","a","e"],["i","h","k","r"],["i","f","l","v"]], words = ["oath","pea","eat","rain"]))
