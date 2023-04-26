3495ec499931546a45bb5c0ab816ae5ed3ab7470
RochaThomas <tomrocha99@gmail.com>
2023-04-26 09:51:26 -0700

    morning algos kthLargest prac heap complete

--- kthLargest.py
+++ kthLargest.py
@@ -6,19 +6,12 @@ import heapq
 
 class Solution:
     def findKthLargest(self, nums, k):
-        k = len(nums) - k
-
-        def quickSelect(l, r):
-            pivot, p =  nums[r], l
-            for i in range(l, r):
-                if nums[i] <= pivot:
-                    nums[p], nums[i] = nums[i], nums[p]
-                    p += 1
-            nums[p], nums[r] = nums[r], nums[p]
-            if p > k: return quickSelect(l, p - 1)
-            elif p < k: return quickSelect(p + 1, r)
-            else: return nums[p]
-
-        return quickSelect(0, len(nums) - 1)
+        for i in range(len(nums)):
+            nums[i] *= -1
+        heapq.heapify(nums)
+        while k > 0:
+            res = heapq.heappop(nums)
+            k -= 1
+        return -res
         
     print(findKthLargest([3,2,1,5,6,4], 2))
