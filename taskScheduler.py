7341ba11c3f05bd022aeb4b26f5edce8c0e6ad45
RochaThomas <tomrocha99@gmail.com>
2023-05-02 11:16:18 -0700

    morning algos task scheduler prac complete hard!

--- taskScheduler.py
+++ taskScheduler.py
@@ -1,8 +1,32 @@
 # morning algos
 # neetcode Task Scheduler
 
+import collections
+import heapq
+
+
 class Solution:
     def leastInterval(self, tasks, n):
-        pass
+        count = collections.Counter(tasks)
+        maxHeap = [-cnt for cnt in count.values()]
+        heapq.heapify(maxHeap)
+
+        time = 0
+
+        # pairs of [-count, idleTime]
+        q = collections.deque()
+
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append([cnt, time + n])
+            
+            if q and q[0][1] == time:
+                heapq.heappush(maxHeap, q.popleft()[0])
+        return time
+
+
 
     print(leastInterval(["A","A","A","B","B","B"], 2))
