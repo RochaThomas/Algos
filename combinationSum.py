68af99767d4ae22c81e043ea2ea1dde6c3dee1ee
RochaThomas <tomrocha99@gmail.com>
2023-05-10 08:43:09 -0700

    morning algos combinationSum prac complete

--- combinationSum.py
+++ combinationSum.py
@@ -3,6 +3,22 @@
 
 class Solution:
     def combinationSum(self, candidates, target):
-        pass
+        res = []
+
+        def dfs(i, curr, total):
+            if total == target:
+                res.append(curr.copy())
+                return
+            if i >= len(candidates) or total > target:
+                return
+
+            curr.append(candidates[i])
+            dfs(i, curr, total + candidates[i])
+            curr.pop()
+            dfs(i + 1, curr, total)
+
+        dfs(0, [], 0)
+        return res
+
 
     print(combinationSum([2,3,6,7], 7))
