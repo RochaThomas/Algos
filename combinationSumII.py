9203b61c6c8faa891c855d547dd48ef0032dd234
RochaThomas <tomrocha99@gmail.com>
2023-05-15 09:51:20 -0700

    morning algos combination sum II  prac complete

--- combinationSumII.py
+++ combinationSumII.py
@@ -3,6 +3,27 @@
 
 class Solution:
     def combinationSum2(self, candidates, target):
-        pass
+        candidates.sort()
+
+        res = []
+
+        def backtrack(curr, pos, target):
+            if target == 0:
+                res.append(curr.copy())
+            if target <= 0:
+                return
+            
+            prev = -1
+            for i in range(pos, len(candidates)):
+                if candidates[i] == prev:
+                    continue
+                curr.append(candidates[i])
+                backtrack(curr, i + 1, target - candidates[i])
+                curr.pop()
+
+                prev = candidates[i]
+
+        backtrack([], 0, target)
+        return res
 
     print(combinationSum2([10,1,2,7,6,1,5], 8))
