22dda2079760a2bfce5afe9fd4ea82d37cf8beef
RochaThomas <tomrocha99@gmail.com>
2023-05-18 08:31:47 -0700

    morning algos letter combinations prac

--- letterCombinations.py
+++ letterCombinations.py
@@ -3,29 +3,6 @@
 
 class Solution:
     def letterCombinations(self, digits):
-        res = []
-        digitToChar = {
-            "2": "abc",
-            "3": "def",
-            "4": "ghi",
-            "5": "jkl",
-            "6": "mno",
-            "7": "pqrs",
-            "8": "tuv",
-            "9": "wxyz",
-        }
-
-        def backtrack(i, curr):
-            if len(curr) == len(digits):
-                res.append(curr[:])
-                return
-
-            for c in digitToChar[digits[i]]:
-                backtrack(i + 1, curr + c)
-
-        if digits:
-            backtrack(0, "")
-        return res
-
+        pass
 
     print(letterCombinations("23"))
