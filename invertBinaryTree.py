2d551ca0448bd2385ed1d17ba39c7b3dfe612163
RochaThomas <tomrocha99@gmail.com>
2023-05-22 09:24:18 -0700

    morning algos invertBinaryTree prac complete

--- invertBinaryTree.py
+++ invertBinaryTree.py
@@ -9,7 +9,13 @@ class TreeNode:
         self.right = right
 class Solution:
     def invertTree(self, root):
-        pass
+        if not root: return
+        temp = root.right
+        root.right = root.left
+        root.left = temp
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
         
 
     print(invertTree([4,2,7,1,3,6,9]))
