faeac80b38d7da370db57cb5acfe297959860090
RochaThomas <tomrocha99@gmail.com>
2023-05-23 08:50:34 -0700

    morning algos maxDepth prac

--- maxDepth.py
+++ maxDepth.py
@@ -9,11 +9,6 @@ class TreeNode:
         self.right = right
 class Solution:
     def maxDepth(self, root):
-        if not root: return 0
-        def helper(node, currDepth):
-            if not node:
-                return currDepth
-            return max(helper(node.left, currDepth + 1), helper(node.right, currDepth + 1))
-        return max(helper(root.left, 1), helper(root.right, 1))
+        pass
     
     print(maxDepth([3,9,20,None,None,15,7]))
