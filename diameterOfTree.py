919737b2fc9ac831f17e9c0bb54fe8b53c8b10f5
RochaThomas <tomrocha99@gmail.com>
2023-05-24 18:38:43 -0700

    morning algos diameterOfTree prac

--- diameterOfTree.py
+++ diameterOfTree.py
@@ -2,23 +2,13 @@
 # neetcode diameter of binary tree
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root):
-        res = [0]
-
-        def dfs(root):
-            if not root:
-                return -1
-            left = dfs(root.left)
-            right = dfs(root.right)
-            res[0] = max(res[0], left + right + 2)
-            return 1 + max(left, right)
-        dfs(root)
-        return res[0]
+        pass
 
     print(diameterOfBinaryTree([1,2,3,4,5]))
