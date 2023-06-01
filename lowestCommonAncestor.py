3426d3e871d37f11b74531c7ffb749428527869d
RochaThomas <tomrocha99@gmail.com>
2023-06-01 10:06:40 -0700

    morning algos lowestCommonAncestor prac

--- lowestCommonAncestor.py
+++ lowestCommonAncestor.py
@@ -10,14 +10,6 @@ class TreeNode:
 
 class Solution:
     def lowestCommonAncestor(self, root, p, q):
-        curr = root
-        while curr:
-            if p.val > curr.val and q.val > curr.val:
-                curr = curr.right
-            elif p.val < curr.val and q.val < curr.val:
-                curr = curr.left
-            else:
-                return curr
-        
+        pass
 
     print(lowestCommonAncestor(root = [6,2,8,0,4,7,9,None,None,3,5], p = 2, q = 8))
