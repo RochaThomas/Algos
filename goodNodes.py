7983c7f8c7627796fa3b40f988512a61a48b33cf
RochaThomas <tomrocha99@gmail.com>
2023-06-06 09:10:39 -0700

    morning algos goodNodes prac

--- goodNodes.py
+++ goodNodes.py
@@ -9,19 +9,6 @@ class TreeNode:
         self.right = right
 class Solution:
     def goodNodes(self, root):
-        """
-        keep track of min in current path
-        if less than min than node is good
-        """
-        def dfs(node, pathMax):
-            if not node:
-                return 0
-            res = 1 if node.val >= pathMax else 0
-            pathMax = max(pathMax, node.val)
-            res += dfs(node.left, pathMax) + dfs(node.right, pathMax)
-            return res
-
-        return dfs(root, root.val)
-            
+        pass
 
     print(goodNodes([3,1,4,3,None,1,5]))
