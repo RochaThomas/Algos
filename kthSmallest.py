e2e9db284d441a22f594d0e45a190311b6f6da05
RochaThomas <tomrocha99@gmail.com>
2023-06-08 07:40:34 -0700

    morning algos kSmallestBST prac

--- kthSmallest.py
+++ kthSmallest.py
@@ -9,24 +9,7 @@ class TreeNode:
         self.right = right
 class Solution:
     def kthSmallest(self, root, k):
-        """
-        list all of the nodes in order from least to greatest
-        when the length of the list == k return the last node
-        push left then curr then right
-        """
-        stack = []
-        count = 0
-        curr = root
-
-        while curr or stack:
-            while curr:
-                stack.append(curr)
-                curr = curr.left
-            curr = stack.pop()
-            count += 1
-            if count == k:
-                return curr.val
-            curr = curr.right
+        pass
 
 
     print(kthSmallest([3,1,4,None,2], 1))
