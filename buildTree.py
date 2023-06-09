9254426874b55c12689c923465f3bdffd844b585
RochaThomas <tomrocha99@gmail.com>
2023-06-09 10:28:49 -0700

    morning algos buildTree prac complete

--- buildTree.py
+++ buildTree.py
@@ -9,6 +9,12 @@ class TreeNode:
         self.right = right
 class Solution:
     def buildTree(self, preorder, inorder):
-        pass
+        if not preorder or not inorder: return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+        root.right =  self.buildTree(preorder[mid + 1:], inorder[mid + 1:])
+        return root
         
     print(buildTree([3,9,20,15,7], [9,3,15,20,7]))
