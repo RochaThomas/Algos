ad7718ef96e36d2c14641a74d8f9b5175d140e10
RochaThomas <tomrocha99@gmail.com>
2023-06-13 09:48:55 -0700

    morning algos serialAndDeserial prac

--- serializeAndDeserialize.py
+++ serializeAndDeserialize.py
@@ -2,11 +2,11 @@
 # neetcode serialize and deserialize binary tree
 
 # Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
 
 class Codec:
 
@@ -16,17 +16,7 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        res = []
-        def dfs(node):
-            if not node: 
-                res.append("N")
-                return
-            res.append((str(node.val)))
-            dfs(node.left)
-            dfs(node.right)
-        dfs(root)
-        return ",".join(res)
-
+        
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -34,19 +24,7 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
-        vals = data.split(",")
-        self.i = 0
-
-        def dfs():
-            if vals[self.i] == "N":
-                self.i += 1
-                return None
-            node = TreeNode(int(vals[self.i]))
-            self.i += 1
-            node.left = dfs()
-            node.right = dfs()
-            return node
-        return dfs()
+        
 
 # Your Codec object will be instantiated and called as such:
 # ser = Codec()
