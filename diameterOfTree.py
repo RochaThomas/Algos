357ddbe3aed3c60c2aae3beff69b05befd61fd85
RochaThomas <tomrocha99@gmail.com>
2023-06-14 10:08:55 -0700

    morning algos diameterOfTree prac complete

--- diameterOfTree.py
+++ diameterOfTree.py
@@ -9,6 +9,16 @@ class TreeNode:
         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root):
-        pass
+        res = [0]
+
+        def dfs(node):
+            if not node: return 0
+            left = dfs(node.left)
+            right = dfs(node.right)
+            res[0] = max(res[0], left + right)
+            return 1 + max(left, right)
+
+        dfs(root)
+        return res[0]
 
     print(diameterOfBinaryTree([1,2,3,4,5]))
