74d977f58cb7f1a5d326da6d5655e1f1c4e461a4
RochaThomas <tomrocha99@gmail.com>
2023-06-14 13:02:07 -0700

    morning algos levelOrder prac complete

--- levelOrder.py
+++ levelOrder.py
@@ -2,6 +2,9 @@
 # neetcode binary tree level order traversal
 
 # Definition for a binary tree node.
+import collections
+
+
 class TreeNode:
     def __init__(self, val=0, left=None, right=None):
         self.val = val
@@ -9,7 +12,22 @@ class TreeNode:
         self.right = right
 class Solution:
     def levelOrder(self, root):
-        pass
+        if not root: return []
+        res = []
+        q = collections.deque()
+        q.append(root)
+
+        while q:
+            qLen = len(q)
+            level = []
+            for i in range(qLen):
+                node = q.popleft()
+                level.append(node.val)
+                if node.left: q.append(node.left)
+                if node.right: q.append(node.right)
+            if level: res.append(level)
+        return res
+
 
     print(levelOrder([3,9,20,None,None,15,7]))
     
