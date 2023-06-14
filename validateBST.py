121d446182a89333e4e74616a90a729bf6b8f752
RochaThomas <tomrocha99@gmail.com>
2023-06-14 13:46:46 -0700

    morning algos validateBST prac

--- validateBST.py
+++ validateBST.py
@@ -9,12 +9,8 @@ class TreeNode:
         self.right = right
 class Solution:
     def isValidBST(self, root):
-        def valid(node, left, right):
-            if not node: return True
-            if not (node.val > left and node.val < right):
-                return False
-            return valid(node.left, left, node.val) and valid(node.right, node.val, right)
-        return valid(root, -float('inf'), float('inf'))
+        pass
+        
 
     print(isValidBST([2,1,3]))
 
