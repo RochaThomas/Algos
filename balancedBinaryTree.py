e275a902512dd8d11062c1b31d672b3399463c94
RochaThomas <tomrocha99@gmail.com>
2023-06-14 10:10:16 -0700

    morning algos balancedBinaryTree prac

--- balancedBinaryTree.py
+++ balancedBinaryTree.py
@@ -9,13 +9,6 @@ class TreeNode:
         self.right = right
 class Solution:
     def isBalanced(self, root):
-        def dfs(node):
-            if not node: return [True, 0]
-            left = dfs(node.left)
-            right = dfs(node.right)
-            balance = left[0] and right[0] and abs(left[1] - right[1]) <= 1
-            return [balance, 1 + max(left[1], right[1])]
-        
-        return dfs(root)[0]
+        pass
 
     print(isBalanced([3,9,20,None,None,15,7]))
