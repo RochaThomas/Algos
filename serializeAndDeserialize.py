17b3ba5ccceb17dcd74f3d17634c77154a2c7bc7
RochaThomas <tomrocha99@gmail.com>
2023-06-15 09:07:29 -0700

    morning algos serialAndDeserial prac complete

--- serializeAndDeserialize.py
+++ serializeAndDeserialize.py
@@ -16,7 +16,17 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        
+        res = []
+        def dfs(node):
+            if not node:
+                res.append('N')
+                return
+            res.append(str(node.val))
+            dfs(node.left)
+            dfs(node.right)
+        dfs(root)
+        print(res)
+        return ','.join(res)
 
     def deserialize(self, data):
         """Decodes your encoded data to tree.
@@ -24,6 +34,21 @@ class Codec:
         :type data: str
         :rtype: TreeNode
         """
+        vals = data.split(',')
+        self.i = 0
+        
+        def dfs():
+            if vals[self.i] == 'N':
+                self.i += 1
+                return None
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs(0)
+            return node
+        return dfs()
+
+
         
 
 # Your Codec object will be instantiated and called as such:
