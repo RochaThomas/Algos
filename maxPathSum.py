60ed95e76a8d2dd4438043594557a8f4b7d9ae7d
RochaThomas <tomrocha99@gmail.com>
2023-06-15 08:27:53 -0700

    morning algos maxPathSum prac

--- maxPathSum.py
+++ maxPathSum.py
@@ -9,17 +9,7 @@ class TreeNode:
         self.right = right
 class Solution:
     def maxPathSum(self, root):
-        # max(maxLeft + maxRight + )
-        res = [root.val]
-        def dfs(node):
-            if not node: return 0
-            left = dfs(node.left)
-            left = max(left, 0)
-            right = dfs(node.right)
-            right = max(right, 0)
-            res[0] = max(res[0], node.val + left + right)
-            return node.val + max(left, right)
-        return max(dfs(root), res[0])
+        pass
         
 
     print(maxPathSum([1,2,3]))
