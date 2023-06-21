7854123de545b0d5e04c869d9e75c11e78f97172
RochaThomas <tomrocha99@gmail.com>
2023-06-21 12:40:50 -0700

    morning algos wordDictionary prac complete

--- wordDictionary.py
+++ wordDictionary.py
@@ -1,16 +1,42 @@
 # morning algos
 # neetcode Design Add and Search Words Data Structure
 
+class TrieNode:
+    def __init__(self) -> None:
+        self.children = {}
+        self.end = False
+
 class WordDictionary:
 
     def __init__(self):
-        
+        self.root = TrieNode()
 
     def addWord(self, word: str) -> None:
-        
+        curr = self.root
+        for letter in word:
+            if letter not in curr.children:
+                curr.children[letter] = TrieNode()
+            curr = curr.children[letter]
+        curr.end = True
 
     def search(self, word: str) -> bool:
-        
+        def dfs(j, root):
+            curr = root
+
+            for i in range(j, len(word)):
+                letter = word[i]
+                if letter == ".":
+                    for child in curr.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if letter not in curr.children:
+                        return False
+                    curr = curr.children[letter]
+            return curr.end
+        return dfs(0, self.root)
+
 
 
 # Your WordDictionary object will be instantiated and called as such:
