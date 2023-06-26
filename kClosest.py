b31ba1d5a4895d80cb213c96922f54e00a23bd0a
RochaThomas <tomrocha99@gmail.com>
2023-06-26 09:25:58 -0700

    morning algos kClosest prac

--- kClosest.py
+++ kClosest.py
@@ -1,24 +1,9 @@
 # morning algos
 # neetcode K Closest Points to Origin
 
-import heapq
-
-
 class Solution:
     def kClosest(self, points, k):
-        
-        minHeap = []
-        for x,y in points:
-            dist = (x**2) + (y**2)
-            minHeap.append([dist,x,y])
-        
-        heapq.heapify(minHeap)
-        res = []
-        while k > 0:
-            dist, x, y = heapq.heappop(minHeap)
-            res.append([x, y])
-            k -= 1
-        return res
+        pass
 
     print(kClosest([[1,3],[-2,2]], 1))
         
