c19cc1a27d8b0f02334dbb7a33ca50f290612c4a
RochaThomas <tomrocha99@gmail.com>
2023-06-27 07:38:04 -0700

    morning algos kthLargest

--- kthLargest.py
+++ kthLargest.py
@@ -1,17 +1,8 @@
 # morning algos
 # neetcode Kth Largest Element in an Array
 
-import heapq
-
-
 class Solution:
     def findKthLargest(self, nums, k):
-        for i in range(len(nums)):
-            nums[i] *= -1
-        heapq.heapify(nums)
-        while k > 0:
-            res = heapq.heappop(nums)
-            k -= 1
-        return -res
+        pass
         
     print(findKthLargest([3,2,1,5,6,4], 2))
