ac40d5e6aac13432d11c20491e7801245deb8f89
RochaThomas <tomrocha99@gmail.com>
2023-07-12 12:15:35 -0700

    morning algos lastStoneWeight prac recap

--- lastStoneWeight.py
+++ lastStoneWeight.py
@@ -1,17 +1,8 @@
 # morning algos
 # neetcode Last Stone Weight
 
-import heapq
-
-
 class Solution:
     def lastStoneWeight(self, stones):
-        stones = [-s for s in stones]
-        heapq.heapify(stones)
-        while len(stones) > 1:
-            s1, s2 = heapq.heappop(stones), heapq.heappop(stones)
-            if s1 != s2: heapq.heappush(stones, s1 - s2)
-
-        return -stones[0] if stones else 0
+        pass
 
     print(lastStoneWeight([2,7,4,1,8,1]))
