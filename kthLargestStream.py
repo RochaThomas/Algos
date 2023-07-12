92e84729a344a1e01d10d1c3a4bed9aeab746f2f
RochaThomas <tomrocha99@gmail.com>
2023-07-12 11:44:13 -0700

    morning algos kthLargestStream prac recap complete

--- kthLargestStream.py
+++ kthLargestStream.py
@@ -1,13 +1,21 @@
 # morning algos
 # neetcode kth largest element in a stream
 
+import heapq
+
+
 class KthLargest:
 
-    def __init__(self, k: int, nums: List[int]):
-        
+    def __init__(self, k, nums):
+        self.k = k
+        self.nums = nums
+        heapq.heapify(self.nums)
 
-    def add(self, val: int) -> int:
-        
+    def add(self, val):
+        if len(self.nums) < self.k or val > self.nums[0]: heapq.heappush(self.nums, val)
+        while len(self.nums) > self.k:
+            heapq.heappop(self.nums)
+        return self.nums[0]
 
 
 # Your KthLargest object will be instantiated and called as such:
