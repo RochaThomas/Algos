31b983465efbc9599053af5501fb01fbeb4d58b6
RochaThomas <tomrocha99@gmail.com>
2023-07-13 08:27:36 -0700

    morning algos subsets prac

--- subsets.py
+++ subsets.py
@@ -3,20 +3,6 @@
 
 class Solution:
     def subsets(self, nums):
-        res = []
+        pass
 
-        subset = []
-        def dfs(i):
-            if i >= len(nums): 
-                res.append(subset.copy())
-                return
-            
-            subset.append(nums[i])
-            dfs(i + 1)
-
-            subset.pop()
-            dfs(i + 1)
-
-        dfs(0)
-        return res
     print(subsets([1,2,3]))
