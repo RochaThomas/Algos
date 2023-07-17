1899382bf4d057c15eb9db48bb24cd1bb00da2f5
RochaThomas <tomrocha99@gmail.com>
2023-07-17 10:33:14 -0700

    morning algos permutations prac

--- permutations.py
+++ permutations.py
@@ -3,20 +3,6 @@
 
 class Solution:
     def permute(self, nums):
-        res = []
-
-        if len(nums) == 1:
-            return [nums[:]]
-        for i in range(len(nums)):
-            n = nums.pop(0)
-            perms = self.permute(nums)
-
-            for perm in perms:
-                perm.append(n)
-            res.extend(perms)
-            nums.append(n)
-    
-        return res
-        
+        pass
 
     print([1,2,3])
