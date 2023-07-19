ffff4e46fe4907e07a4b17c3c749a63137ee379e
RochaThomas <tomrocha99@gmail.com>
2023-07-19 09:13:52 -0700

    morning algos subsetsII prac complete

--- subsetsII.py
+++ subsetsII.py
@@ -3,7 +3,26 @@
 
 class Solution:
     def subsetsWithDup(self, nums):
-        pass
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i >= len(nums):
+                res.append(subset[:])
+                return
+            
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            
+            subset.pop()
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
+
 
 
     print(subsetsWithDup([1,2,2]))
