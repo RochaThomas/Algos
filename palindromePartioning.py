edda73a7c1ec08d16e5acf9da91aecc00dd2d3fd
RochaThomas <tomrocha99@gmail.com>
2023-07-27 20:35:25 -0700

    morning algos palindromePartitioning prac recap

--- palindromePartioning.py
+++ palindromePartioning.py
@@ -3,29 +3,6 @@
 
 class Solution:
     def partition(self, s):
-        res = []
-        part = []
-
-        def isPalindrome(s, l, r):
-            while l < r:
-                if s[l] != s[r]:
-                    return False
-                l += 1
-                r -= 1
-            return True
-        
-        def backtrack(i):
-            if i >= len(s):
-                res.append(part[:])
-                return
-            for j in range(i, len(s)):
-                if isPalindrome(s, i, j):
-                    part.append(s[i:j+1])
-                    backtrack(j + 1)
-                    part.pop()
-        backtrack(0)
-        return res
-
-
+        pass
 
     print(partition("aab"))
