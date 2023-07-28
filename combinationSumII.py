3f083b96219f7b2132479dc80b8bd72e2ba88e8d
RochaThomas <tomrocha99@gmail.com>
2023-07-28 09:26:17 -0700

    morning algos comboSumII prac recap 2x

--- combinationSumII.py
+++ combinationSumII.py
@@ -3,25 +3,6 @@
 
 class Solution:
     def combinationSum2(self, candidates, target):
-        candidates.sort()
-        res = []
-
-        def backtrack(pos, curr, total):
-            if total == target:
-                res.append(curr[:])
-            if pos >= len(candidates) or total >= target:
-                return
-            
-            prev = -1
-            for i in range(pos, len(candidates)):
-                if candidates[i] == prev:
-                    continue
-                curr.append(candidates[i])
-                backtrack(i + 1, curr, total + candidates[i])
-                curr.pop()
-                prev = candidates[i]
-
-        backtrack(0, [], 0)
-        return res
+        pass
 
     print(combinationSum2([10,1,2,7,6,1,5], 8))
