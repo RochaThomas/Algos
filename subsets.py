81b3bfa295bfeff587294d084ff6f3f70fb5a301
RochaThomas <tomrocha99@gmail.com>
2023-07-28 08:12:14 -0700

    morning algos subsets prac recap 2x

--- subsets.py
+++ subsets.py
@@ -3,21 +3,6 @@
 
 class Solution:
     def subsets(self, nums):
-        res = []
-
-        curr = []
-        def backtrack(i):
-            if i >= len(nums):
-                res.append(curr[:])
-                return
-            
-            curr.append(nums[i])
-            backtrack(i + 1)
-
-            curr.pop()
-            backtrack(i + 1)
-        
-        backtrack(0)
-        return res
+        pass
 
     print(subsets([1,2,3]))
