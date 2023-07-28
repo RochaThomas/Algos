68536ea826b45d34a7511731e37e625c2a3e9aa2
RochaThomas <tomrocha99@gmail.com>
2023-07-28 08:45:08 -0700

    morning algos comSum prac recap 2x complete

--- combinationSum.py
+++ combinationSum.py
@@ -3,6 +3,18 @@
 
 class Solution:
     def combinationSum(self, candidates, target):
-        pass
+        res = []
+        curr = []
+        def backtrack(i, total):
+            if total == target:
+                res.append(curr[:])
+            if total >= target or i >= len(candidates):
+                return
+            curr.append(candidates[i])
+            backtrack(i, total + candidates[i])
+            curr.pop()
+            backtrack(i + 1, total)
+        backtrack(0, 0)
+        return res
 
     print(combinationSum([2,3,6,7], 7))
