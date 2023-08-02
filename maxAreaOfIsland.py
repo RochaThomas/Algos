aa5fba7e89855c08b2517c8a633e02323e7a3c6e
RochaThomas <tomrocha99@gmail.com>
2023-08-02 08:41:20 -0700

    morning algos maxIslandArea

--- maxAreaOfIsland.py
+++ maxAreaOfIsland.py
@@ -3,30 +3,7 @@
 
 class Solution:
     def maxAreaOfIsland(self, grid):
-        if not grid:
-            return 0
-
-        ROWS, COLS = len(grid), len(grid[0])
-        visit = set()
-        maxArea = 0
-
-        def dfs(r, c):
-            if (r < 0 or r == ROWS or c < 0 or c == COLS or
-                grid[r][c] == 0 or (r, c) in visit):
-                return 0
-            
-            visit.add((r, c))
-            return (1 + dfs(r + 1, c) +
-                        dfs(r, c + 1) +
-                        dfs(r - 1, c) +
-                        dfs(r, c - 1))
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                maxArea = max(maxArea, dfs(r, c))
-        
-        return maxArea
-
+        pass
 
     print(maxAreaOfIsland([
         [0,0,1,0,0,0,0,1,0,0,0,0,0],
