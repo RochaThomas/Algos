9edcf7871d3b9d256201745ef3b91785b681537b
RochaThomas <tomrocha99@gmail.com>
2023-08-04 10:41:07 -0700

    morning algos surroundedRegions complete

--- surroundedRegions.py
+++ surroundedRegions.py
@@ -6,7 +6,33 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
-        pass
+        ROWS, COLS = len(board), len(board[0])
+        noFlip = set()
+
+        def search(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] == 'X' or (r, c) in noFlip:
+                return
+            if board[r][c] == 'O':
+                noFlip.add((r, c))
+                search(r + 1, c)
+                search(r - 1, c)
+                search(r, c + 1)
+                search(r, c - 1)
+
+        for r in range(ROWS):
+            search(r, 0)
+            search(r, COLS - 1)
+        for c in range(1, COLS - 1):
+            search(0, c)
+            search(ROWS - 1, c)
+
+        print(noFlip)
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == 'O' and (r, c) not in noFlip:
+                    board[r][c] = 'X'
+
         
     print(solve([
         ["X","X","X","X"],
