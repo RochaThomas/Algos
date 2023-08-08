8ecd0419681272b2935c39319583813e746e7e6f
RochaThomas <tomrocha99@gmail.com>
2023-08-08 12:59:16 -0700

    morning algos wallsAndGates

---
+++ wallsAndGates.py
@@ -0,0 +1,12 @@
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param rooms: m x n 2D grid
+    @return: nothing
+    """
+    def walls_and_gates(self, rooms):
+        # write your code here
+        pass
