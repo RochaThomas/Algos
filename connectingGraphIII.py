57b609ba2f428dc83f6d6add46516fd0c9b8a01c
RochaThomas <tomrocha99@gmail.com>
2023-08-16 10:34:10 -0700

    morning algos connectingGraphIII complete

--- connectingGraphIII.py
+++ connectingGraphIII.py
@@ -1,23 +1,31 @@
 # morning algos 
 # neetcode Number of Connected Components in an Undirected Graph
 
-class ConnectingGraph3:
-    """
-    @param a: An integer
-    @param b: An integer
-    @return: nothing
-    """
-    def __init__(self, n):
-        # initialize your data structure here.
-        pass
-    
-    def connect(self, a, b):
-        # write your code here
-        pass
+class Solution:
+    def countComponents(self, n, edges):
+        par = [i for i in range(n)]
+        rank = [1] * n
 
-    """
-    @return: An integer
-    """
-    def query(self):
-        # write your code here
-        pass
+        def find(n1):
+            res = n1
+            while res != par[res]:
+                par[res] = par[par[res]]
+                res = par[res]
+            return res
+        
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
+                return 0
+            if rank[p2] > rank[p1]:
+                par[p1] = p2
+                rank[p2] += rank[p1]
+            else:
+                par[p2] = p1
+                rank[p1] += rank[p2]
+            return 1
+        
+        res = n
+        for n1, n2 in edges:
+            res -= union(n1, n2)
+        return res
