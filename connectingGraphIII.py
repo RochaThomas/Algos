7cd90ca934781f92be3d8e1dd1463515744d91a9
RochaThomas <tomrocha99@gmail.com>
2023-08-16 09:55:20 -0700

    morning algos connectingGraphIII

---
+++ connectingGraphIII.py
@@ -0,0 +1,23 @@
+# morning algos 
+# neetcode Number of Connected Components in an Undirected Graph
+
+class ConnectingGraph3:
+    """
+    @param a: An integer
+    @param b: An integer
+    @return: nothing
+    """
+    def __init__(self, n):
+        # initialize your data structure here.
+        pass
+    
+    def connect(self, a, b):
+        # write your code here
+        pass
+
+    """
+    @return: An integer
+    """
+    def query(self):
+        # write your code here
+        pass
