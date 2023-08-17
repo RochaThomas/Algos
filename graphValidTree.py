5c463ac1bc0b2da4c79707479989ba6ddf56ab5f
RochaThomas <tomrocha99@gmail.com>
2023-08-17 08:17:37 -0700

    morning algos graphValidTree

---
+++ graphValidTree.py
@@ -0,0 +1,16 @@
+# morning algos
+# neetcode Graph Valid Tree
+
+from typing import (
+    List,
+)
+
+class Solution:
+    """
+    @param n: An integer
+    @param edges: a list of undirected edges
+    @return: true if it's a valid tree, or false
+    """
+    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+        # write your code here
+        pass
