9dd649afe2f6e3d0890ef1e779eaacf2fbe45d35
RochaThomas <tomrocha99@gmail.com>
2023-08-21 10:58:17 -0700

    morning algos numOfIslands prac

--- numberOfIslands.py
+++ numberOfIslands.py
@@ -3,28 +3,7 @@
 
 class Solution:
     def numIslands(self, grid):
-        numOfIslands = 0
-        visited = set()
-        ROWS, COLS = len(grid), len(grid[0])
-
-        def search(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r,c) in visited or grid[r][c] == '0':
-                return False
-            visited.add((r,c))
-            return (
-                search(r + 1, c) or
-                search(r - 1, c) or
-                search(r, c + 1) or
-                search(r, c - 1)
-            )
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r,c) in visited: continue
-                elif grid[r][c] == '1': 
-                    numOfIslands += 1
-                    search(r, c)
-        return numOfIslands
+        pass
 
     print(numIslands([
         ["1","1","1","1","0"],
