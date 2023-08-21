848859b3d05c039da2dd4e0d5b699edcdb413107
RochaThomas <tomrocha99@gmail.com>
2023-08-21 14:07:57 -0700

    morning algos cloneGraph prac

--- cloneGraph.py
+++ cloneGraph.py
@@ -1,27 +1,8 @@
 # morning algos
 # neetcode Clone Graph
 
-
-# Definition for a Node.
-class Node:
-    def __init__(self, val = 0, neighbors = None):
-        self.val = val
-        self.neighbors = neighbors if neighbors is not None else []
-
 class Solution:
     def cloneGraph(self, node):
-        oldToNew = {}
-
-        def clone(node):
-            if node in oldToNew:
-                return oldToNew[node]
-            
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            for neighbor in node.neighbors:
-                copy.neighbors.append(clone(neighbor))
-            return copy
-        return clone(node) if node else None
-
+        pass
 
     print(cloneGraph([[2,4],[1,3],[2,4],[1,3]]))
