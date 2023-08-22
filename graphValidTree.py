5a51d329f356c8b26062a1a6ac63ff091420a495
RochaThomas <tomrocha99@gmail.com>
2023-08-22 13:38:16 -0700

    morning algos graphValidTree prac

--- graphValidTree.py
+++ graphValidTree.py
@@ -11,24 +11,7 @@ class Solution:
     @param edges: a list of undirected edges
     @return: true if it's a valid tree, or false
     """
-    def valid_tree(self, n, edges):
-        if not n:
-            return True
-        adj = {i:[] for i in range(n)}
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
-        visited = set()
-
-        def dfs(i, prev):
-            if i in visited:
-                return False
-            visited.add(i)
-            for j in adj[i]:
-                if j == prev:
-                    continue
-                if not dfs(j, i):
-                    return False
-            return True
-        return dfs(0, -1) and n == len(visited)
+    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
+        # write your code here
+        pass
 
