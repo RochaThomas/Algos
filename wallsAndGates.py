c7b09ec771cb6a506c20e029f7139ac1b71fed23
RochaThomas <tomrocha99@gmail.com>
2023-08-21 20:00:14 -0700

    morning algos wallsAndGates prac recap

--- wallsAndGates.py
+++ wallsAndGates.py
@@ -1,7 +1,6 @@
 # morning algos
 # neetcode walls and gates
 
-import collections
 from typing import (
     List,
 )
@@ -11,32 +10,8 @@ class Solution:
     @param rooms: m x n 2D grid
     @return: nothing
     """
-    def walls_and_gates(self, rooms: List[List[int]]):
-        ROWS, COLS = len(rooms), len(rooms[0])
-        visited = set()
-        q = collections.deque()
-
-        for r in range(ROWS):
-            for c in range(COLS):
-                if rooms[r][c] == 0:
-                    q.append([r, c])
-                    visited.add((r, c))
-
-        def addRoom(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or rooms[r][c] == -1:
-                return 
-            visited.add((r, c))
-            q.append([r, c])
-
-        distance = 0
-        while q:
-            for i in range(len(q)):
-                r, c = q.popleft()
-                rooms[r][c] = distance
-                addRoom(r + 1, c)
-                addRoom(r - 1, c)
-                addRoom(r, c + 1)
-                addRoom(r, c - 1)
-            distance += 1
+    def walls_and_gates(self, rooms):
+        # write your code here
+        pass
 
 
