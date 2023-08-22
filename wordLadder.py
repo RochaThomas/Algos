28f55577dc4ce77254a8379b4a74dca535cbe978
RochaThomas <tomrocha99@gmail.com>
2023-08-22 14:37:15 -0700

    morning algo wordLadder prac complete

--- wordLadder.py
+++ wordLadder.py
@@ -2,8 +2,36 @@
 # neetcode Word Ladder
 
 
+import collections
+
+
 class Solution:
     def ladderLength(self, beginWord, endWord, wordList):
-        pass
+        if endWord not in wordList:
+            return 0
+        nei = collections.defaultdict(list)
+        for word in wordList:
+            for i in range(len(word)):
+                pattern = word[:i] + "*" + word[i + 1:]
+                nei[pattern].append(word)
+
+        visit = set([beginWord])
+        q = collections.deque([beginWord])
+        res = 1
+        
+        while q:
+            for i in range(len(q)):
+                word = q.popleft()
+                if word == endWord:
+                    return res
+                for i in range(len(word)):
+                    pattern = word[:i] + "*" + word[i + 1:]
+                    for neiWord in nei[pattern]:
+                        if neiWord not in visit:
+                            visit.add(neiWord)
+                            q.append(neiWord)
+            res += 1
+        return 0
+
 
     print(ladderLength("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
