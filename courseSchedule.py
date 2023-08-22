ffd8220a19fb81f634748bd92b46cb3a6bfc00cd
RochaThomas <tomrocha99@gmail.com>
2023-08-22 09:16:50 -0700

    morning algos courseScheduler prac complete

--- courseSchedule.py
+++ courseSchedule.py
@@ -3,6 +3,29 @@
 
 class Solution:
     def canFinish(self, numCourses, prerequisites):
-        pass
+        preReqMap = {i:[] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            preReqMap[course].append(pre)
+
+        visit = set()
+
+        def dfs(course):
+            if course in visit:
+                return False
+            if preReqMap[course] == []:
+                return True
+            
+            visit.add(course)
+
+            for pre in preReqMap[course]:
+                if not dfs(pre):
+                    return False
+            visit.remove(course)
+            preReqMap[course] = []
+            return True
+        
+        for course in range(numCourses):
+            if not dfs(course): return False
+        return True
 
     print(canFinish(2, [[1,0],[0,1]]))
