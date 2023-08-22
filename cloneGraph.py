f61cfeefea8ff29931c288d1f3311efbcfc36405
RochaThomas <tomrocha99@gmail.com>
2023-08-21 20:09:13 -0700

    morning algos cloneGraph prac recap 2x

--- cloneGraph.py
+++ cloneGraph.py
@@ -10,19 +10,6 @@ class Node:
 
 class Solution:
     def cloneGraph(self, node):
-        nodeMap = {}
-
-        def dfs(node):
-            if node in nodeMap:
-                return nodeMap[node]
-            copy = Node(node.val)
-            nodeMap[node] = copy
-            for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
-
-        return dfs(node) if node else None
-
-
+        pass
 
     print(cloneGraph([[2,4],[1,3],[2,4],[1,3]]))
