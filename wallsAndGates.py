a965e0d634089461850d66157a273200527ef5b4
RochaThomas <tomrocha99@gmail.com>
2023-08-21 17:10:51 -0700

    morning algos wallsAndGates prac complete

--- wallsAndGates.py
+++ wallsAndGates.py
@@ -1,3 +1,7 @@
+# morning algos
+# neetcode walls and gates
+
+import collections
 from typing import (
     List,
 )
@@ -7,32 +11,32 @@ class Solution:
     @param rooms: m x n 2D grid
     @return: nothing
     """
-    def walls_and_gates(self, rooms):
+    def walls_and_gates(self, rooms: List[List[int]]):
         ROWS, COLS = len(rooms), len(rooms[0])
         visited = set()
-        q = []
+        q = collections.deque()
 
-        def addRoom(r, c):
-            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or rooms[r][c] == -1:
-                return
-            visited.add((r, c))
-            q.append([r, c])
-
-        
         for r in range(ROWS):
             for c in range(COLS):
                 if rooms[r][c] == 0:
-                    q.append([r,c])
-                    visited.add((r,c))
+                    q.append([r, c])
+                    visited.add((r, c))
 
-        dist = 0
+        def addRoom(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or rooms[r][c] == -1:
+                return 
+            visited.add((r, c))
+            q.append([r, c])
+
+        distance = 0
         while q:
             for i in range(len(q)):
-                r, c = q.pop(0)
-                rooms[r][c] = dist
+                r, c = q.popleft()
+                rooms[r][c] = distance
                 addRoom(r + 1, c)
                 addRoom(r - 1, c)
-                addRoom(r, c - 1)
                 addRoom(r, c + 1)
-            dist += 1
-        
+                addRoom(r, c - 1)
+            distance += 1
+
+
