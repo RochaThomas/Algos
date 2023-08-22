25dc73dd9f549233f8fb593818cd0b3139ea4c11
RochaThomas <tomrocha99@gmail.com>
2023-08-21 20:28:33 -0700

    morning algo walllsAndGates prac recap 2x complete

--- wallsAndGates.py
+++ wallsAndGates.py
@@ -1,6 +1,7 @@
 # morning algos
 # neetcode walls and gates
 
+import collections
 from typing import (
     List,
 )
@@ -11,4 +12,29 @@ class Solution:
     @return: nothing
     """
     def walls_and_gates(self, rooms):
-        pass
+        ROWS, COLS = len(rooms), len(rooms[0])
+        visited = set()
+        distance = 0
+        q = collections.deque()
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if rooms[r][c] == 0:
+                    q.append([r, c])
+                    visited.add((r, c))
+        
+        def addRoom(r, c):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in visited or rooms[r][c] == -1:
+                return
+            q.append([r, c])
+            visited.add((r, c))
+
+        while q:
+            for i in range(len(q)):
+                r, c = q.popleft()
+                rooms[r][c] = distance
+                addRoom(r + 1, c)
+                addRoom(r - 1, c)
+                addRoom(r, c + 1)
+                addRoom(r, c - 1)
+            distance += 1
