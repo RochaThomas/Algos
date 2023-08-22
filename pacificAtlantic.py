9f188833a1dd38ea56b7631ee957cf5dcded02d9
RochaThomas <tomrocha99@gmail.com>
2023-08-21 17:33:15 -0700

    morning algos pacificAtlantic prac recap complete

--- pacificAtlantic.py
+++ pacificAtlantic.py
@@ -3,7 +3,36 @@
 
 class Solution:
     def pacificAtlantic(self, heights):
-        pass
+        # run dfs for every pacific and dfs for every atlantic
+        # run through all points in grid and see if they are in both
+        # retrun points in both
+        pac, atl = set(), set()
+        ROWS, COLS = len(heights), len(heights[0])
+
+        def dfs(r, c, ocean, prev):
+            if r < 0 or c < 0 or r >= ROWS or c >= COLS or (r, c) in ocean or heights[r][c] < prev:
+                return
+            ocean.add((r, c))
+            dfs(r + 1, c, ocean, heights[r][c])
+            dfs(r - 1, c, ocean, heights[r][c])
+            dfs(r, c + 1, ocean, heights[r][c])
+            dfs(r, c - 1, ocean, heights[r][c])
+
+        for r in range(ROWS):
+            dfs(r, 0, pac, 0)
+            dfs(r, COLS - 1, atl, 0)
+
+        for c in range(COLS):
+            dfs(0, c, pac, 0)
+            dfs(ROWS - 1, c, atl, 0)
+        
+        res = []
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if (r, c) in pac and (r, c) in atl:
+                    res.append([r, c])
+        return res
 
     print(pacificAtlantic([
         [1,2,2,3,5],
