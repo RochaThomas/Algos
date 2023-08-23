6406a5d1b4e1860822f0f7242b2e839219aca641
RochaThomas <tomrocha99@gmail.com>
2023-08-22 19:20:24 -0700

    morning algos connectGraphIII prac recap complete

--- connectingGraphIII.py
+++ connectingGraphIII.py
@@ -3,17 +3,18 @@
 
 class Solution:
     def countComponents(self, n, edges):
-        par = [i for i in range(1, n + 1)]
+        par = [i for i in range(n + 1)]
         rank = [1] * (n + 1)
+
+        connected = n
 
         def find(n):
             p = par[n]
             while p != par[p]:
                 par[p] = par[par[p]]
                 p = par[p]
-
             return p
-
+        
         def union(n1, n2):
             p1, p2 = find(n1), find(n2)
             if p1 == p2:
@@ -24,10 +25,7 @@ class Solution:
             else:
                 par[p1] = p2
                 rank[p2] += rank[p1]
-
             return 1
-        
-        connected = n
 
         for n1, n2 in edges:
             connected -= union(n1, n2)
