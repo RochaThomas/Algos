ed31e9b7b10c8391d693a4d33187e5c61df6d28c
RochaThomas <tomrocha99@gmail.com>
2023-08-22 19:44:06 -0700

    morning algos wordLadder prac recap complete

--- wordLadder.py
+++ wordLadder.py
@@ -9,14 +9,16 @@ class Solution:
     def ladderLength(self, beginWord, endWord, wordList):
         if endWord not in wordList:
             return 0
-        nei = collections.defaultdict(list)
+        
+        patterns = collections.defaultdict(list)
+        q = collections.deque([beginWord])
+        visit = set([beginWord])
+
         for word in wordList:
             for i in range(len(word)):
                 pattern = word[:i] + "*" + word[i + 1:]
-                nei[pattern].append(word)
+                patterns[pattern].append(word)
 
-        visit = set([beginWord])
-        q = collections.deque([beginWord])
         res = 1
         
         while q:
@@ -24,12 +26,14 @@ class Solution:
                 word = q.popleft()
                 if word == endWord:
                     return res
+
                 for i in range(len(word)):
                     pattern = word[:i] + "*" + word[i + 1:]
-                    for neiWord in nei[pattern]:
-                        if neiWord not in visit:
-                            visit.add(neiWord)
-                            q.append(neiWord)
+                    for w in patterns[pattern]:
+                        if w in visit:
+                            continue
+                        visit.add(w)
+                        q.append(w)
             res += 1
         return 0
 
