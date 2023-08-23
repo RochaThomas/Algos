ff148cd580674009dd36a0233b2df36206b0e296
RochaThomas <tomrocha99@gmail.com>
2023-08-22 20:00:04 -0700

    morning algo courseScheduleII prac recap2 complete

--- courseScheduleII.py
+++ courseScheduleII.py
@@ -3,6 +3,32 @@
 
 class Solution:
     def findOrder(self, numCourses, prerequisites):
-        pass
+        preMap = {i:[] for i in range(numCourses)}
+        for course, pre in prerequisites:
+            preMap[course].append(pre)
+
+        res = []
+        cycle = set()
+        taken = set()
+
+        def dfs(course):
+            if course in cycle:
+                return False
+            if course in taken:
+                return True
+            cycle.add(course)
+            for pre in preMap[course]:
+                if not dfs(pre): 
+                    return False
+            cycle.remove(course)
+            taken.add(course)
+            res.append(course)
+            return True
+
+        for i in range(numCourses):
+            if not dfs(i):
+                return []
+        return res
+
 
     print(findOrder(2, [[1,0]]))
