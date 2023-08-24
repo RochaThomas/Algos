89807e2aa9a2462d5651b3deb2734886cd13a192
RochaThomas <tomrocha99@gmail.com>
2023-08-24 07:58:29 -0700

    morning algos minCostConnectPoints

--- minCostConnectPoints.py
+++ minCostConnectPoints.py
@@ -1,36 +1,8 @@
 # morning algos
 # neetcode Min Cost to Connect All Points
 
-import heapq
-
-
 class Solution:
     def minCostConnectPoints(self, points):
-        N = len(points)
-
-        adj = { i:[] for i in range(N) }
-
-        for i in range(N):
-            x1, y1 = points[i]
-            for j in range(i + 1, N):
-                x2, y2 = points[j]
-                dist = abs(x1 - x2) + abs(y1 - y2)
-                adj[i].append([dist, j])
-                adj[j].append([dist, i])
-        
-        res = 0
-        visit = set()
-        minH = [[0,0]]
-        while len(visit) < N:
-            cost, i = heapq.heappop(minH)
-            if i in visit:
-                continue
-            res += cost
-            visit.add(i)
-            for neiCost, nei in adj[i]:
-                if nei not in visit:
-                    heapq.heappush(minH, [neiCost, nei])
-        return res
-
+        pass
 
     print(minCostConnectPoints([[0,0],[2,2],[3,10],[5,2],[7,0]]))
