1887dc616f2f6fcfb23f6547f4ff224ca1f8bf0b
RochaThomas <tomrocha99@gmail.com>
2023-08-31 15:53:39 -0700

    morning algos networkDelay prac recap 2x

--- networkDelay.py
+++ networkDelay.py
@@ -1,29 +1,9 @@
 # morning algos
 # neetcode Network Delay Time
 
-import heapq
-
-
 class Solution:
     def networkDelayTime(self, times, n, k):
-        adj = {i:[] for i in range(1, n + 1)}
-        for src, dst, time in times:
-            adj[src].append([dst, time])
-        
-        minHeap = [[0, k]]
-        time = 0
-        visit = set()
-
-        while minHeap:
-            t, node = heapq.heappop(minHeap)
-            if node in visit:
-                continue
-            visit.add(node)
-            time = max(t, time)
-            for nei, w in adj[node]:
-                if nei not in visit:
-                    heapq.heappush(minHeap, [t + w, nei])
-        return time if len(visit) == n else -1
+        pass
             
 
 
