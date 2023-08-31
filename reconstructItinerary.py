12312d7f444c2306c5e19fe78a50a4428d972856
RochaThomas <tomrocha99@gmail.com>
2023-08-31 15:53:11 -0700

    morning algo reconstItinera prac recap 2x complete

--- reconstructItinerary.py
+++ reconstructItinerary.py
@@ -3,7 +3,29 @@
 
 class Solution:
     def findItinerary(self, tickets):
-        pass
+        tickets.sort()
+        adj = {src:[] for src, dest in tickets}
+        for src, dest in tickets:
+            adj[src].append(dest)
+
+        res = ['JFK']
+
+        def dfs(src):
+            if len(res) == len(tickets) + 1:
+                return True
+            if src not in adj:
+                return False
+            temp = list(adj[src])
+            for i, v in enumerate(temp):
+                res.append(v)
+                adj[src].pop(i)
+                if dfs(v): return True
+
+                adj[src].insert(i, v)
+                res.pop()
+            return False
+        dfs('JFK')
+        return res
             
 
 
