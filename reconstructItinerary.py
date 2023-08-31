e7a6f83ef080b18e40fef3a087ce69b6daf694f4
RochaThomas <tomrocha99@gmail.com>
2023-08-31 10:42:45 -0700

    morning algos reconstructItinerary

--- reconstructItinerary.py
+++ reconstructItinerary.py
@@ -6,29 +6,6 @@ import collections
 
 class Solution:
     def findItinerary(self, tickets):
-        tickets.sort()
-        ticketMap = {src:[] for src, dest in tickets}
-        for src, dest in tickets:
-            ticketMap[src].append(dest)
-
-        res = ['JFK']
-
-        def dfs(src):
-            if len(res) == len(tickets) + 1:
-                return True
-            if src not in ticketMap:
-                return False
-            temp = list(ticketMap[src])
-            for i, v in enumerate(temp):
-                ticketMap[src].pop(i)
-                res.append(v)
-                if dfs(v):
-                    return True
-                ticketMap[src].insert(i, v)
-                res.pop()
-            return False
-        
-        dfs('JFK')
-        return res
+        pass
 
     print(findItinerary([["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]))
