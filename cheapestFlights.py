6b1299988a187476bdfb93a20d001135c431102b
RochaThomas <tomrocha99@gmail.com>
2023-09-01 12:05:48 -0700

    morning algos cheapestFlights prac recap

--- cheapestFlights.py
+++ cheapestFlights.py
@@ -3,20 +3,7 @@
 
 class Solution:
     def findCheapestPrice(self, n, flights, src, dst, k):
-        prices = [float('inf')] * n
-        prices[src] = 0
-
-        for i in range(k + 1):
-            temp = prices.copy()
-            for s, d, p in flights:
-                if prices[s] == float('inf'):
-                    continue
-                if prices[s] + p < temp[d]:
-                    temp[d] = prices[s] + p
-            prices = temp
-
-
-        return prices[dst] if prices[dst] != float('inf') else -1
+        pass
 
 
 
