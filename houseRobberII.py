9515a3d3eba3c152874c3dbb83c7a761113e4f62
RochaThomas <tomrocha99@gmail.com>
2023-09-08 07:55:32 -0700

    morning algos houseRobberII

--- houseRobberII.py
+++ houseRobberII.py
@@ -3,18 +3,6 @@
 
 class Solution:
     def rob(self, nums):
-        
-        return max(nums[0], self.helper(nums[1:]), self.helper(nums[:-1]))
-
-    def helper(self, nums):
-        rob1, rob2 = 0, 0
-
-        for n in nums:
-            temp = max(n + rob1, rob2)
-            rob1 = rob2
-            rob2 = temp
-        
-        return rob2
-
+        pass
 
     print(rob([2,3,2]))
