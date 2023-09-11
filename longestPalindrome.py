8654e1c9efa6c184d9874f774b9ec8e6b0c31cae
RochaThomas <tomrocha99@gmail.com>
2023-09-11 08:18:38 -0700

    morning algos longestPalindrome

--- longestPalindrome.py
+++ longestPalindrome.py
@@ -3,28 +3,6 @@
 
 class Solution:
     def longestPalindrome(self, s):
-        res = ""
-        resLen = 0
+        pass
 
-        for i in range(len(s)):
-            # check odd length first
-            l, r = i, i
-            while l >= 0 and r < len(s) and s[l] == s[r]: 
-                newLen = r - l + 1
-                if newLen > resLen:
-                    resLen = newLen
-                    res = s[l:r+1]
-                r += 1
-                l -= 1
-
-            # check even length
-            l, r = i, i + 1
-            while l >= 0 and r < len(s) and s[l] == s[r]: 
-                newLen = r - l + 1
-                if newLen > resLen:
-                    resLen = newLen
-                    res = s[l:r+1]
-                r += 1
-                l -= 1
-        return res
     print(longestPalindrome("babad"))
