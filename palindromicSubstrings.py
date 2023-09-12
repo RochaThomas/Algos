d32f8ec2be66c17c2b6dec2841c5b4ce2b2277b7
RochaThomas <tomrocha99@gmail.com>
2023-09-12 08:08:37 -0700

    morning algos palindromicSubstrings complete

--- palindromicSubstrings.py
+++ palindromicSubstrings.py
@@ -3,7 +3,17 @@
 
 class Solution:
     def countSubstrings(self, s):
-        pass
+        subs = 0
+        for i in range(len(s)):
+            subs += self.countPali(s, i, i) + self.countPali(s, i, i + 1)
+        return subs
+    def countPali(self, s, l, r):
+        res = 0
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            res += 1
+            l -= 1
+            r += 1
+        return res
 
 
     print(countSubstrings("abc"))
