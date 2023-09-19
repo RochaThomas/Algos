89bad3a0bf6e4404100ae1b232801d0f69e5ca6f
RochaThomas <tomrocha99@gmail.com>
2023-09-19 08:51:25 -0700

    morning algos longestIncreasingSeq complete

--- longestIncreasingSubsequence.py
+++ longestIncreasingSubsequence.py
@@ -3,4 +3,13 @@
 
 class Solution:
     def lengthOfLIS(self, nums):
-        pass
+        if not nums: return 0
+        res = 1
+        subLens = [1] * len(nums)
+
+        for i in range(len(nums) - 1, -1, -1):
+            for j in range(i + 1, len(nums)):
+                if nums[i] < nums[j]:
+                    subLens[i] = max(subLens[i], 1 + subLens[j])
+                    res = max(res, subLens[i])
+        return res
