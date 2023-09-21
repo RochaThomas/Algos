68a97063cd5b775bd246a429981c8c4f852eb582
RochaThomas <tomrocha99@gmail.com>
2023-09-21 08:22:34 -0700

    morning algos minCostClimbStair prac complete

--- minCostClimbingStairs.py
+++ minCostClimbingStairs.py
@@ -3,6 +3,10 @@
 
 class Solution:
     def minCostClimbingStairs(self, cost):
-        pass
+        cost.append(0)
+
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
 
     print(minCostClimbingStairs([10,15,20]))
