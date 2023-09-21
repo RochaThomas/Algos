5eeef60f7f2450dbe723c0c8b9cb8b5d4707cada
RochaThomas <tomrocha99@gmail.com>
2023-09-21 07:36:35 -0700

    morning algos houseRobber prac

--- houseRobber.py
+++ houseRobber.py
@@ -3,12 +3,6 @@
 
 class Solution:
     def rob(self, nums):
-        start0, start1 = 0, 0
-        for n in nums:
-            temp = max(n + start0, start1)
-            start0 = start1
-            start1 = temp
-        
-        return start1
+        pass
 
     print(rob([1,2,3,1]))
