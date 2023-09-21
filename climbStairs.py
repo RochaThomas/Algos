4218598d4f2f3d3cc77feb3e0a1838add3a77a0d
RochaThomas <tomrocha99@gmail.com>
2023-09-21 07:59:37 -0700

    morning algos climbStairs prac complete

--- climbStairs.py
+++ climbStairs.py
@@ -3,6 +3,12 @@
 
 class Solution:
     def climbStairs(self, n):
-        pass
+        one, two = 1, 1
+        for i in range(n - 1):
+            temp = one
+            one = one + two
+            two = temp
+        return one
+
 
     print(climbStairs(2))
