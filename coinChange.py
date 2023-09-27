98a1b6cd791c2b81d769e6473f959ab4184cbd75
RochaThomas <tomrocha99@gmail.com>
2023-09-27 09:51:02 -0700

    morning algos coinChange prac recap 2x

--- coinChange.py
+++ coinChange.py
@@ -3,14 +3,7 @@
 
 class Solution:
     def coinChange(self, coins, amount):
-        dp = [amount + 1] * (amount + 1)
-        dp[0] = 0
-
-        for a in range(1, amount + 1):
-            for coin in coins:
-                if a - coin >= 0:
-                    dp[a] = min(1 + dp[a - coin], dp[a])
-        return dp[amount] if dp[amount] != amount + 1 else -1
+        pass
 
 
     print(coinChange([1,2,5], 11))
