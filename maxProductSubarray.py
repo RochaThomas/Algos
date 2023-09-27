00f26750b92ec68e17b0da9067cb4569c3c4cb2b
RochaThomas <tomrocha99@gmail.com>
2023-09-27 09:49:43 -0700

    morning algos maxProdSubarray prac recap complete

--- maxProductSubarray.py
+++ maxProductSubarray.py
@@ -3,7 +3,18 @@
 
 class Solution:
     def maxProduct(self, nums):
-        pass
+        currMin, currMax = 1, 1
+        res = -float('inf')
+
+        for n in nums:
+            if n == 0:
+                currMin = currMax = 1
+                continue
+            temp = currMax * n
+            currMax = max(currMax * n, currMin * n, n)
+            currMin = min(temp, currMin * n, n)
+            res = max(res, currMax)
+        return res
 
 
     print(maxProduct([2,3,-2,4]))
