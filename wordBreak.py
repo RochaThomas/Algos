be1ec7f2fba6c6eb32f7f25a12231de0167bb948
RochaThomas <tomrocha99@gmail.com>
2023-09-28 10:02:45 -0700

    morning algos wordBreak prac complete

--- wordBreak.py
+++ wordBreak.py
@@ -3,6 +3,17 @@
 
 class Solution:
     def wordBreak(self, s, wordDict):
-        pass
+
+        dp = [False] * (len(s) + 1)
+        dp[len(s)] = True
+        
+        for i in range(len(s) - 1, -1, -1):
+            for word in wordDict:
+                if dp[i]:
+                    break
+                if i + len(word) - 1 < len(s) and s[i : i + len(word)] == word:
+                    dp[i] = dp[i + len(word)]
+        return dp[0]
+
 
     print(wordBreak( "leetcode", ["leet","code"]))
