9c40ae23de4d54eed2db85f8a533027181a3505c
RochaThomas <tomrocha99@gmail.com>
2023-09-28 10:22:57 -0700

    morning algos partitionSubsetsum prac complete

--- partitionSubsetSum.py
+++ partitionSubsetSum.py
@@ -3,6 +3,18 @@
 
 class Solution:
     def canPartition(self, nums):
-        pass
+        if sum(nums) % 2: return False
+
+        target = sum(nums) // 2
+        dp = set()
+        dp.add(0)
+        for n in nums:
+            nextDP = set()
+            for val in dp:
+                if n + val == target: return True
+                nextDP.add(val)
+                nextDP.add(val + n)
+                dp = nextDP
+        return False
 
     print(canPartition([1,5,11,5]))
