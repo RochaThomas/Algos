0e93657953e50b34d0b0cb359b87fbe8691096e5
RochaThomas <tomrocha99@gmail.com>
2023-10-17 12:28:52 -0700

    morning algos coinChangeII prac recap 2x complete

--- coinChangeII.py
+++ coinChangeII.py
@@ -3,7 +3,16 @@
 
 class Solution:
     def change(self, amount, coins):
-        pass
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        
+        for i in range(len(coins) - 1, -1, -1):
+            nextDP = dp
+            for a in range(1, amount + 1):
+                if a - coins[i] >= 0:
+                    nextDP[a] += nextDP[a - coins[i]]
+            dp = nextDP
+        return dp[amount]
 
 
     print(change(5, [1,2,5]))
