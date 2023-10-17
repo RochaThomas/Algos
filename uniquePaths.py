5d1860391ecd4de81c57188f4e30e4964e4e671f
RochaThomas <tomrocha99@gmail.com>
2023-10-17 08:37:14 -0700

    morning algos uniquePaths prac

--- uniquePaths.py
+++ uniquePaths.py
@@ -3,15 +3,6 @@
 
 class Solution:
     def uniquePaths(self, m, n):
-        dp = [[0] * n] * m
-        dp[0][0] = 1
-
-        for r in range(m):
-            for c in range(n):
-                if r == 0 or c == 0:
-                    dp[r][c] = 1
-                else:
-                    dp[r][c] = dp[r - 1][c] + dp[r][c -1]
-        return dp[m - 1][n - 1]
+        pass
 
     print(uniquePaths(3, 7))
