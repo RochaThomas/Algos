22f88a70084c087438f315e5a83d82ced21f2e39
RochaThomas <tomrocha99@gmail.com>
2023-10-17 11:43:06 -0700

    morning algos maxProfit prac recap

--- maxProfit.py
+++ maxProfit.py
@@ -3,23 +3,7 @@
 
 class Solution:
     def maxProfit(self, prices):
-        dp  = {}
-        
-        def dfs(i, buying, curr):
-            if i >= len(prices):
-                return 0
-            if (i, buying) in dp:
-                return dp[(i, buying)]
-            
-            cooldown = dfs(i + 1, buying)
-            if buying:
-                buy = dfs(i + 1, not buying) - prices[i]
-                dp[(i, buying)] = max(buy, cooldown)
-            else:
-                sell = dfs(i + 2, not buying) + prices[i]
-                dp[(i, buying)] = max(sell, cooldown)
-            return dp[(i, buying)]
-        return dp[(0, True)]
+        pass
 
 
     print(maxProfit([7,1,5,3,6,4]))
