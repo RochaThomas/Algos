3b475333e63c0409c00964880236aac16cff2b16
RochaThomas <tomrocha99@gmail.com>
2023-10-18 11:18:56 -0700

    morning algos targetSum prac recap

--- targetSum.py
+++ targetSum.py
@@ -3,13 +3,6 @@
 
 class Solution:
     def findTargetSumWays(self, nums, target):
-        dp = {}
-
-        def dfs(i, total):
-            if i >= len(nums): return 1 if total == target else 0
-            if (i, total) in dp: return dp[(i, total)]
-            dp[(i, total)] = dfs(i + 1, total - nums[i]) + dfs(i + 1, total + nums[i])
-            return dp[(i, total)]
-        return dfs(0, 0)
+        pass
 
     print(findTargetSumWays([1,1,1,1,1], 3))
