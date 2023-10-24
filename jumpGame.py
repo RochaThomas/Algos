baa742eda33e8219cb38ae674ab4fd1f2333966e
RochaThomas <tomrocha99@gmail.com>
2023-10-24 07:47:24 -0700

    morning algos jumpGame complete

--- jumpGame.py
+++ jumpGame.py
@@ -3,6 +3,12 @@
 
 class Solution:
     def canJump(self, nums):
-        pass
+        maxIdx = 0
+
+        for i in range(len(nums)):
+            maxIdx = max(maxIdx, i + nums[i])
+            if maxIdx >= len(nums) - 1: return True
+            if maxIdx == i: return False
+        return False
 
     print(canJump([2,3,1,1,4]))
