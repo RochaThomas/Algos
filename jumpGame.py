1d99a24ccd62ecd1c898c370be1d25acb97149a0
RochaThomas <tomrocha99@gmail.com>
2023-10-24 07:16:20 -0700

    morning algos jumpGame

--- jumpGame.py
+++ jumpGame.py
@@ -3,13 +3,6 @@
 
 class Solution:
     def canJump(self, nums):
-        goal = len(nums) - 1
-
-        for i in range(len(nums) - 2, -1, -1):
-            if i + nums[i] >= goal:
-                goal = i
-            
-        return True if goal == 0 else False
-
+        pass
 
     print(canJump([2,3,1,1,4]))
