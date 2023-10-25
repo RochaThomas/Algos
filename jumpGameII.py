03137709967e2894ad5cdef193e149398adb0f0c
RochaThomas <tomrocha99@gmail.com>
2023-10-25 09:16:02 -0700

    morning algos jumpGameII complete

--- jumpGameII.py
+++ jumpGameII.py
@@ -3,6 +3,16 @@
 
 class Solution:
     def jump(self, nums):
-        pass
+        res = 0
+        l = r = 0
+
+        while r < len(nums) - 1:
+            farthest = 0
+            for i in range(l, r + 1):
+                farthest = max(farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            res += 1
+        return res
 
     print(jump([2,3,1,1,4]))
