0fe1d67205a9b46f4dee8358bda6558fa5797f2e
RochaThomas <tomrocha99@gmail.com>
2023-11-02 07:51:35 -0700

    morning algos maxSubarray prac complete

--- maximumSubarray.py
+++ maximumSubarray.py
@@ -3,7 +3,16 @@
 
 class Solution:
     def maxSubArray(self, nums):
-        pass
+        res = nums[0]
+        curr = 0
+
+        for n in nums:
+            if curr < 0:
+                curr = 0
+            curr += n
+            res = max(res, curr)
+        return res
+
 
 
     print(maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
