d5e18324df557dd08fa2c577cc1a37a1038ff46e
RochaThomas <tomrocha99@gmail.com>
2023-11-03 09:04:02 -0700

    morning algos mergeTriplets prac

--- mergeTriplets.py
+++ mergeTriplets.py
@@ -3,15 +3,7 @@
 
 class Solution:
     def mergeTriplets(self, triplets, target):
-        good = set()
-
-        for t in triplets:
-            if t[0] > target[0] or t[1] > target[1] or t[2] > target[2]:
-                continue
-            for i, v in enumerate(t):
-                if v == target[i]:
-                    good.add(i)
-        return len(good) == 3
+        pass
 
 
     print(mergeTriplets([[2,5,3],[1,8,4],[1,7,5]], [2,7,5]))
