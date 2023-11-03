202444f6a50742c2fede400db5a16a58a113b806
RochaThomas <tomrocha99@gmail.com>
2023-11-03 09:26:15 -0700

    morning algos partitionLabels prac

--- partitionLabels.py
+++ partitionLabels.py
@@ -3,20 +3,6 @@
 
 class Solution:
     def partitionLabels(self, s):
-        letterIndex  = {}
-        res = []
-
-        for i, v in enumerate(s):
-            letterIndex[v] = i
-
-        end = 0
-        size = 0
-        for i, c in enumerate(s):
-            size += 1
-            end = max(end, letterIndex[c])
-            if i == end:
-                res.append(size)
-                size = 0
-        return res
+        pass
 
     print(partitionLabels("ababcbacadefegdehijhklij"))
