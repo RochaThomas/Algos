4c29879ec939248ac82a660243d1b74dbf676392
RochaThomas <tomrocha99@gmail.com>
2023-11-08 07:24:21 -0800

    morning algos nonOverlapInter

--- nonOverlappingIntervals.py
+++ nonOverlappingIntervals.py
@@ -3,17 +3,6 @@
 
 class Solution:
     def eraseOverlapIntervals(self, intervals):
-        intervals.sort()
-
-        res = 0
-        prevEnd = intervals[0][1]
-        for start, end in intervals[1:]:
-            if start >= prevEnd:
-                prevEnd = end
-            else:
-                res += 1
-                prevEnd = min(end, prevEnd)
-
-        return res
+        pass
 
     print(eraseOverlapIntervals([[1,2],[2,3],[3,4],[1,3]]))
