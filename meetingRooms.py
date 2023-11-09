e778e3f72ccf02bd7d0b55c470a3ba47d86cbb05
RochaThomas <tomrocha99@gmail.com>
2023-11-09 09:04:42 -0800

    morning algos meetingRooms complete

--- meetingRooms.py
+++ meetingRooms.py
@@ -3,4 +3,10 @@
 
 class Solution:
     def can_attend_meetings(self, intervals) -> bool:
-        pass
+        intervals.sort()
+        prev = intervals[0][0] - 1
+        for start, end in intervals:
+            if start < prev:
+                return False
+            prev = end
+        return True
