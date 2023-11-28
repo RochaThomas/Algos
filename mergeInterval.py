17f203674281d6c0fcd549bf9ca20eca1028432b
RochaThomas <tomrocha99@gmail.com>
2023-11-28 08:52:12 -0800

    morning algos merge prac complete

--- mergeInterval.py
+++ mergeInterval.py
@@ -3,6 +3,17 @@
 
 class Solution:
     def merge(self, intervals):
-        pass
+        res = []
+        curr = intervals[0]
+
+        for s, e in intervals:
+            if curr[1] < s:
+                res.append(curr)
+                curr = [s, e]
+            elif curr[1] >= s:
+                curr = max(curr[1], e)
+        res.append(curr)
+        return res
+
 
     print(merge([[1,3],[2,6],[8,10],[15,18]]))
