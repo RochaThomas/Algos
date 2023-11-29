75cbff8301121c938b47221dcf7fa1584203cffc
RochaThomas <tomrocha99@gmail.com>
2023-11-29 09:20:37 -0800

    morning algos meetingRoomsII prac

--- meetingRoomsII.py
+++ meetingRoomsII.py
@@ -3,17 +3,4 @@
 
 class Solution:
     def min_meeting_rooms(self, intervals) -> int:
-        start = sorted([i.start for i in intervals])
-        end = sorted([i.end for i in intervals])
-
-        res, count = 0, 0
-        s, e = 0, 0
-        while s < len(intervals):
-            if start[s] < end[e]:
-                s += 1
-                count += 1
-            else:
-                e += 1
-                count += 1
-            res = max(res, count)
-        return res
+        pass
