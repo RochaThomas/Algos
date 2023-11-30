78493a544b026f6246f48f79a1f7a35f7959b1c8
RochaThomas <tomrocha99@gmail.com>
2023-11-30 09:22:42 -0800

    morning algos rotateImage complete

--- rotateImage.py
+++ rotateImage.py
@@ -3,6 +3,18 @@
 
 class Solution:
     def rotate(self, matrix):
-        pass
+        l, r = 0, len(matrix) - 1
+
+        while l < r:
+            for i in range(r - l):
+                t, b = l, r
+                topLeft = matrix[t][l + i]
+                matrix[t][l + i] = matrix[b - i][l]
+                matrix[b - i][l] = matrix[b][r - i]
+                matrix[b][r - i] = matrix[t + i][r]
+                matrix[t + i][r] = topLeft
+            l += 1
+            r -= 1
+
 
     print(rotate([[1,2,3],[4,5,6],[7,8,9]]))
