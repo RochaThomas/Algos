8941a9f3d4563f46d0b3e84a1303780fa06cf8ab
RochaThomas <tomrocha99@gmail.com>
2023-12-04 09:24:26 -0800

    morning algos setMatrixZeroes complete

--- setMatrixZeroes.py
+++ setMatrixZeroes.py
@@ -3,6 +3,25 @@
 
 class Solution:
     def setZeroes(self, matrix):
-        pass
-
+        ROWS, COLS = len(matrix), len(matrix[0])
+        rowZero = 1
+        for r in range(ROWS):
+            for c in range(COLS):
+                if matrix[r][c] == 0:
+                    matrix[0][c] = 0
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZero = 0
+        for r in range(1, ROWS):
+            for c in range(1, COLS):
+                if matrix[0][c] == 0 or matrix[r][0] == 0:
+                    matrix[r][c] = 0
+        if matrix[0][0] == 0:
+            for r in range(ROWS):
+                matrix[r][0] = 0
+        if rowZero == 0:
+            for c in range(COLS):
+                matrix[0][c] = 0
+                
     print(setZeroes([[1,1,1],[1,0,1],[1,1,1]]))
