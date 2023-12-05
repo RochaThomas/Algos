9d64aa4b7417099e3918f30b83ad6d0a7bafc76d
RochaThomas <tomrocha99@gmail.com>
2023-12-05 08:26:34 -0800

    morning algos happyNumber complete

--- happyNumber.py
+++ happyNumber.py
@@ -3,6 +3,24 @@
 
 class Solution:
     def isHappy(self, n):
-        pass
+        cache = set()
+
+        res = self.calc(n)
+        while True:
+            if res == 1:
+                return True
+            elif res in cache:
+                return False
+            else:
+                cache.add(res)
+                res = self.calc(res)
+        
+    def calc(self, num):
+        res = 0
+        while num > 0:
+            digit = num % 10
+            res += digit * digit
+            num = num // 10
+        return res
 
     print(isHappy(19))
