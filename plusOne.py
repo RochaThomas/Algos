253e87cba9435591399038b778e5d33ba938fa90
RochaThomas <tomrocha99@gmail.com>
2023-12-06 08:27:21 -0800

    morning algos plusOne complete

--- plusOne.py
+++ plusOne.py
@@ -3,6 +3,13 @@
 
 class Solution:
     def plusOne(self, digits):
-        pass
+        carry = 1
+        for i in range(len(digits) - 1, -1, -1):
+            curr = digits[i] + carry
+            digits[i] = curr % 10
+            if curr < 10:
+                return digits
+        return [carry] + digits
+
 
     print(plusOne([1,2,3]))
