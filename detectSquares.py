b08510212c8ef75cc33351672abe1fcc00d81e68
RochaThomas <tomrocha99@gmail.com>
2023-12-22 09:41:40 -0800

    morning algos detectrSquares prac

--- detectSquares.py
+++ detectSquares.py
@@ -1,24 +1,13 @@
 # morning algos 
 # neetcode Detect Squares
 
-import collections
-
-
 class DetectSquares:
 
     def __init__(self):
-        self.ptsCount = collections.defaultdict(int)
-        self.pts = []
+        pass
 
     def add(self, point):
-        self.ptsCount[tuple(point)] += 1
-        self.pts.append(point)
+        pass
 
     def count(self, point):
-        res = 0
-        px, py = point
-        for x, y in self.pts:
-            if (abs(py - y) != abs(px - x)) or x == px or y == py:
-                continue
-            res += self.ptsCount[(x, py)] * self.ptsCount[(px, y)]
-        return res
+        pass
