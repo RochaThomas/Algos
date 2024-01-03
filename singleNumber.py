4d8880a5952e507a877c05b2b4539666b898ed1d
RochaThomas <tomrocha99@gmail.com>
2024-01-03 11:09:01 -0800

    morning algos singleNumber

--- singleNumber.py
+++ singleNumber.py
@@ -3,9 +3,6 @@
 
 class Solution:
     def singleNumber(self, nums):
-        res = 0
-        for n in nums:
-            res = n ^ res
-        return res
+        pass
 
     print(singleNumber([2,2,1]))
