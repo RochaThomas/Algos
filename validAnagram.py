413fd49d13df97e36c35ba318bb1dfa40aaa693f
RochaThomas <tomrocha99@gmail.com>
2024-04-12 10:46:41 -0700

    morning algos validAnagram complete

--- validAnagram.py
+++ validAnagram.py
@@ -3,6 +3,19 @@
 
 class Solution:
     def isAnagram(self, s, t):
-        pass
+        if len(s) != len(t): return False
+        sCount = {}
+        tCount = {}
+        for i in range(len(s)):
+            sCount[s[i]] = sCount.get(s[i], 0) + 1
+            tCount[t[i]] = tCount.get(t[i], 0) + 1
+
+        for key in sCount.keys():
+            if sCount[key] != tCount.get(key, 0):
+                return False
+        return True
+
+        # simple solution
+        return sorted(s) == sorted(t)
 
     print(isAnagram("anagram", "nagaram"))
