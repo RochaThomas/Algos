99b906c78ee287c8a66988dda8cd909b422719bc
RochaThomas <tomrocha99@gmail.com>
2024-04-13 12:46:04 -0700

    morning algos twoSum prac complete

--- twoSum.py
+++ twoSum.py
@@ -9,6 +9,11 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-        pass
+        n = {}
+        for i in range(len(nums)):
+            if target - nums[i] in n:
+                return [i, n[target - nums[i]]]
+            n[nums[i]] = i
+        return False
 
     print(twoSum([2,7,11,15], 9))
