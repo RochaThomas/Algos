3ffff448302edafd59c06b17aa5c676c9afcc1a5
RochaThomas <tomrocha99@gmail.com>
2024-04-15 09:37:03 -0700

    morning algos groupAnagrams prac complete

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -3,8 +3,32 @@
 # morning algos
 # neetcode Group Anagrams
 
+import collections
+
+
 class Solution:
     def groupAnagrams(strs):
-        pass
+        # anagramDict = {}
+        # for s in strs:
+        #     k = ",".join(sorted(s))
+        #     if k in anagramDict:
+        #         anagramDict[k].append(s)
+        #     else:
+        #         anagramDict[k] = [s]
+        # return anagramDict.values()
+
+        res = collections.defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord("a")] += 1
+
+            res[tuple(count)].append(s)
+
+        print(res)
+
+        return res.values()
+
+        
 
     print(groupAnagrams(strs = ["eat","tea","tan","ate","nat","bat"]))
