4a8a1e8397be1882ca8eeaf43c220dcaecca786e
RochaThomas <tomrocha99@gmail.com>
2024-04-18 09:38:11 -0700

    morning algos isValidSudoku prac complete

--- isValidSudoku.py
+++ isValidSudoku.py
@@ -3,9 +3,28 @@
 # morning algos
 # neet code Valid Sudoku
 
+import collections
+
+
 class Solution:
     def isValidSudoku(board):
-        pass
+        cols = collections.defaultdict(set)
+        rows = collections.defaultdict(set)
+        squares = collections.defaultdict(set)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == '.':
+                    continue
+                if (board[r][c] in rows[r] or
+                    board[r][c] in cols[c] or
+                    board[r][c] in squares[(r // 3, c // 3)]):
+                    return False
+                
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+        return True
 
     print(isValidSudoku([
     [".",".","4",".",".",".","6","3","."],
