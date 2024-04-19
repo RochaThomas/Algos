8cd5969590cea746b91bbf6debb7b63318cd9f3a
RochaThomas <tomrocha99@gmail.com>
2024-04-19 09:59:16 -0700

    morning algos longestConsecSeq prac complete

--- longestConsecutiveSequence.py
+++ longestConsecutiveSequence.py
@@ -3,6 +3,16 @@
 
 class Solution:
     def longestConsecutive(self, nums):
-        pass
-    
+        numSet = set(nums)
+
+        longest = 0
+
+        for n in nums:
+            if (n - 1) not in numSet:
+                length = 0
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
+
     print(longestConsecutive([100,4,200,1,3,2]))
