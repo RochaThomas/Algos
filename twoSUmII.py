29bcea2b6255a1475aacffde30adce7a93d3d7be
RochaThomas <tomrocha99@gmail.com>
2024-04-24 14:25:53 -0700

    morning algos twoSumII prac complete

--- twoSUmII.py
+++ twoSUmII.py
@@ -3,7 +3,16 @@
 
 class Solution:
     def twoSum(self, numbers, target):
-        pass
+        l, r = 0, len(numbers) - 1
+        while l < r:
+            tSum = numbers[l] + numbers[r]
+            if tSum == target:
+                return [l + 1, r + 1]
+            elif tSum > target:
+                r -= 1
+            else:
+                l += 1
+            
 
     print(twoSum([2,7,11,15], 9))
     
