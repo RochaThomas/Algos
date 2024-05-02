ba2cf0cd1d4ab703c0384f35ac3081c22056a2e3
RochaThomas <tomrocha99@gmail.com>
2024-05-02 10:41:05 -0700

    morning algos trapRainWater prac complete

--- trappingRainWater.py
+++ trappingRainWater.py
@@ -4,7 +4,20 @@
 
 class Solution:
     def trap(self, height):
-        pass
+        if not height: return 0
+        l, r = 0, len(height) - 1
+        maxL, maxR = height[l], height[r]
+        res = 0
+        while l < r:
+            if maxL < maxR:
+                l += 1
+                maxL = max(maxL, height[l])
+                res += maxL - height[l]
+            else:
+                r -= 1
+                maxR = max(maxR, height[r])
+                res += maxR - height[r]
+        return res
 
     print("rain trapped", trap([0,1,0,2,1,0,1,3,2,1, 2, 1]))
 #                           0 1 2 3 4 5 6 7 8 9 10 11
