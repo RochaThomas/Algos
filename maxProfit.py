4e7d64150627e3f53ea24798c873ff83a9d5c92b
RochaThomas <tomrocha99@gmail.com>
2024-05-03 13:53:01 -0700

    morning algos maxProfit prac complete

--- maxProfit.py
+++ maxProfit.py
@@ -3,6 +3,13 @@
 
 class Solution:
     def maxProfit(self, prices):
-        pass
+        buy = float("inf")
+        res = 0
+        for i in range(len(prices)):
+            if prices[i] < buy:
+                buy = prices[i]
+            else:
+                res = max(res, prices[i] - buy)
+        return res
 
     print(maxProfit([7,1,5,3,6,4]))
