714d86c9a2e95147269e87957a40936eb7f55311
RochaThomas <tomrocha99@gmail.com>
2024-05-07 11:36:32 -0700

    morning algos longestSubstring prac complete

--- longestSubstring.py
+++ longestSubstring.py
@@ -4,7 +4,19 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s):
-        pass
+        if not s: return 0
+        letters = set()
+        l, r = 0, 1
+        res = 1
+        letters.add(s[l])
 
-
+        while r < len(s):
+            while s[r] in letters:
+                letters.remove(s[l])
+                l += 1
+            letters.add(s[r])
+            r += 1
+            res = max(res, r - l)
+        return res
+        
     print(lengthOfLongestSubstring("abcabcbb"))
