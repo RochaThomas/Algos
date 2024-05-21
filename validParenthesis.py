8631de6a6ba712bfd0d2753356b7cfc62c588509
RochaThomas <tomrocha99@gmail.com>
2024-05-21 08:31:30 -0700

    morning algos validParenth prac

--- validParenthesis.py
+++ validParenthesis.py
@@ -3,21 +3,6 @@
 
 class Solution:
     def isValid(self, s):
-        stack = []
-        closedToOpen = {')':'(', ']':'[', '}':'{'}
-
-        if len(s) % 2 != 0 or s[0] in closedToOpen.keys() or s[-1] in closedToOpen.values(): return False
-        
-        for c in s:
-            if c in closedToOpen:
-                if stack and stack[-1] == closedToOpen[c]:
-                    stack.pop()
-                else:
-                    return False
-            else:
-                stack.append(c)
-
-        return True if not stack else False
-
+        pass
 
     print(isValid("()"))
