73b4d3c327dd86db9dcce6963b94f07c7ffa1d5f
RochaThomas <tomrocha99@gmail.com>
2024-05-21 08:55:10 -0700

    morning algos validParen prac complete

--- validParenthesis.py
+++ validParenthesis.py
@@ -3,6 +3,16 @@
 
 class Solution:
     def isValid(self, s):
-        pass
+        paren = {'(':')', '{':'}', '[':']'}
+        stack = []
+        
+        for i in range(len(s)):
+            if s[i] not in paren:
+                if not stack: return False
+                compare = stack.pop()
+                if paren[compare] != s[i]: return False
+            else:
+                stack.append(s[i])
+        return True if not stack else False
 
     print(isValid("()"))
