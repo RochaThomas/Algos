0ec6ff9adfe3b6317a46ae17dd056d6693309cd0
RochaThomas <tomrocha99@gmail.com>
2024-05-23 12:51:29 -0700

    morning algos minStack prac complete

--- minStack.py
+++ minStack.py
@@ -3,16 +3,24 @@
 
 class MinStack:
     def __init__(self):
-        pass
+        self.stack = []
+        self.min = float('inf')
 
     def push(self, val: int) -> None:
-        pass
+        self.stack.append(val)
+        if val < self.min:
+            self.min = val
 
     def pop(self) -> None:
-        pass
+        val = self.stack.pop()
+        if val == self.min:
+            newMin = float('inf')
+            for v in self.stack:
+                newMin = min(newMin, v)
+            self.min = newMin
 
     def top(self) -> int:
-        pass
+        return self.stack[-1]
 
     def getMin(self) -> int:
-        pass
+        return self.min
