9704dd97f8cfe32ddafb392da7a2d25df0631bee
RochaThomas <tomrocha99@gmail.com>
2024-05-24 10:53:17 -0700

    morning algos evalRPN prac

--- evalRPN.py
+++ evalRPN.py
@@ -3,22 +3,7 @@
 
 
 def evalRPN(tokens):
-    nums = []
-    for c in tokens:
-        if c == "+":
-            nums.append(nums.pop() + nums.pop())
-        elif c == "-":
-            n1, n2 = nums.pop(), nums.pop()
-            nums.append(n2 - n1)
-        elif c == "*":
-            nums.append(nums.pop() * nums.pop())
-        elif c == "/":
-            n1, n2 = nums.pop(), nums.pop()
-            nums.append(int(n2 / n1))
-        else:
-            nums.append(int(c))
-
-    return nums[0]
+    pass
 
 
 print(evalRPN(["10","6","9","3","+","-11","*","/","*","17","+","5","+"]))
