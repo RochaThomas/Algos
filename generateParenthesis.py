49cbc9b140d4ef90cc7b077ac19655c4a3335b35
RochaThomas <tomrocha99@gmail.com>
2024-05-29 07:55:26 -0700

    morning algos genParen prac complete

--- generateParenthesis.py
+++ generateParenthesis.py
@@ -3,6 +3,22 @@
 
 class Solution:
     def generateParenthesis(self, n):
-        pass
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+                return
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res
 
     print(generateParenthesis(3))
