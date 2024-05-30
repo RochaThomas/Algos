1b89f6b463fb363bf43b937f490f1b08eda9dfd0
RochaThomas <tomrocha99@gmail.com>
2024-05-30 08:30:29 -0700

    morning algos dailyTemp prac complete

--- dailyTemperatures.py
+++ dailyTemperatures.py
@@ -3,6 +3,15 @@
 
 class Solution:
     def dailyTemperatures(self, temperatures):
-        pass
+        stack = []
+        res = [0] * len(temperatures)
+
+        for i in range(len(temperatures)):
+            while stack and temperatures[i] > stack[-1][0]:
+                val, idx = stack.pop()
+                res[idx] = i - idx
+            stack.append([temperatures[i], i])
+        return res
+
 
     print(dailyTemperatures([73,74,75,71,69,72,76,73]))
