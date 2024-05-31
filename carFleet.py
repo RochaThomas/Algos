2ee53639c7d4bec305416caa125583bf08ddbd81
RochaThomas <tomrocha99@gmail.com>
2024-05-31 09:10:13 -0700

    morning algos carFleet prac complete

--- carFleet.py
+++ carFleet.py
@@ -3,6 +3,14 @@
 
 class Solution:
     def carFleet(self, target, position, speed):
-        pass
+        pairs = [[p, s] for p, s in zip(position, speed)]
+        res = []
+
+        for p, s in sorted(pairs)[::-1]:
+            t = (target - p) / s
+            if not res or res[-1] < t:
+                res.append(t)
+            
+        return len(res)
 
     print(carFleet(10, [6,8], [3,2]))
