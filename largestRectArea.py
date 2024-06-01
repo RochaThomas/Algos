f4d2471846fe90d5203097a55c82d7336943c6fa
RochaThomas <tomrocha99@gmail.com>
2024-06-01 11:21:42 -0700

    morning algos largestRect prac

--- largestRectArea.py
+++ largestRectArea.py
@@ -4,21 +4,6 @@
 
 class Solution:
     def largestRectangleArea(self, heights):
-        maxArea = 0
-        # stack holds a pair (index, height)
-        stack = []
-
-        for i, h in enumerate(heights):
-            start = i
-            while stack and stack[-1][1] > h:
-                index, height = stack.pop()
-                maxArea = max(maxArea, height * ( i - index))
-                start = index
-            stack.append((start, h))
-
-        for i, h in stack:
-            maxArea = max(maxArea, h * (len(heights) - i))
-
-        return maxArea
+        pass
 
     print(largestRectangleArea([2,1,5,6,2,3]))
