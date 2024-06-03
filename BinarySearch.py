ec8a1c90bdd53cd73bffd558676f5297dc2aed93
RochaThomas <tomrocha99@gmail.com>
2024-06-03 09:27:23 -0700

    morning algos binarySearch prac

--- BinarySearch.py
+++ BinarySearch.py
@@ -3,17 +3,6 @@
 
 class Solution:
     def search(self, nums, target):
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            midpoint = l + ((r - l) // 2)
-            if nums[midpoint] == target:
-                return midpoint
-            elif nums[midpoint] < target:
-                l = midpoint + 1
-            else:
-                r = midpoint - 1
-        
-        return -1
+        pass
 
     print(search([5], 5))
