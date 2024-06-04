58f2046696a1b4f064d5c4c7e6f4f0752c50d51a
RochaThomas <tomrocha99@gmail.com>
2024-06-04 08:57:49 -0700

    morning algos searchMatrix prac complete

--- searchMatrix.py
+++ searchMatrix.py
@@ -3,6 +3,26 @@
 
 class Solution:
     def searchMatrix(self, matrix, target):
-        pass
+        t, b = 0, (len(matrix) - 1)
+        while t <= b:
+            rowIdx = (t + b) // 2
+            if target > matrix[rowIdx][-1]:
+                t = rowIdx + 1
+            elif target < matrix[rowIdx][0]:
+                b = rowIdx - 1
+            else:
+                l, r = 0, (len(matrix[rowIdx]) - 1)
+                while l <= r:
+                    m = (l + r) // 2
+                    if matrix[rowIdx][m] < target:
+                        l = m + 1
+                    elif matrix[rowIdx][m] > target:
+                        r = m - 1
+                    else:
+                        return True
+                return False
+        return False
+
+
 
     print(searchMatrix([[1,3,5,7],[10,11,16,20],[23,30,34,60]], 60))
