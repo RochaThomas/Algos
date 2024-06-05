0a0b009eedc385699b651eda0796c7b4751a1d74
RochaThomas <tomrocha99@gmail.com>
2024-06-05 09:13:10 -0700

    morning algos minEatingSpeed prac complete

--- minEatingSpeed.py
+++ minEatingSpeed.py
@@ -2,8 +2,27 @@
 # neetcode koko eating bananas
 
 
+import math
+
+
 class Solution:
     def minEatingSpeed(self, piles, h):
-        pass
-    
+        l, r = 1, max(piles)
+        res = r
+
+        while l <= r:
+            k = (l + r) // 2
+            hours = 0
+            for p in piles:
+                hours += math.ceil(p / k)
+                if hours > h:
+                    l = k + 1
+                    break
+            if hours <= h:
+                res = min(res, k)
+                r = k - 1
+        return res
+
+
+
     print(minEatingSpeed([3, 6, 7, 11], 8))
