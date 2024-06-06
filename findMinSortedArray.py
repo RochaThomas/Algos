02d724ddc0d4bd73616d03d44a9877f032bfb5aa
RochaThomas <tomrocha99@gmail.com>
2024-06-06 09:25:52 -0700

    morning algos findMinSortedArray prac

--- findMinSortedArray.py
+++ findMinSortedArray.py
@@ -3,24 +3,6 @@
 
 class Solution:
     def findMin(self, nums):
-        """
-        everything to the left must be less than the right
-        run binary search to find where the value to the immediate left it greater than the right
-        that value is the min
-        """
-        if len(nums) == 1:
-            return nums[0]
-        
-        l, r = 0, len(nums) - 1
-
-        while l <= r:
-            m = l + ((r - l) // 2)
-            if nums[m - 1] > nums[m]:
-                return nums[m]
-            if nums[m] > nums[r]:
-                l = m + 1
-            else:
-                r = m - 1
-        return -1
+        pass
 
     print(findMin([3,4,5,1,2]))
