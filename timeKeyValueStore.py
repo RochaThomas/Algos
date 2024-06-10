ac2989430d4a95547c5dcc41e0706940bf71d0fa
RochaThomas <tomrocha99@gmail.com>
2024-06-10 11:04:01 -0700

    morning algos timeKeyVal prac complete

--- timeKeyValueStore.py
+++ timeKeyValueStore.py
@@ -4,13 +4,31 @@
 class TimeMap:
 
     def __init__(self):
-        pass
+        self.map = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
-        pass
+        if key not in self.map:
+            self.map[key] = [[value, timestamp]]
+        else:
+            self.map[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        pass
+        if key not in self.map: return ""
+        res = ""
+        vals = self.map[key]
+        l, r = 0, len(vals) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if vals[m][1] < timestamp:
+                res = vals[m][0]
+                l = m + 1
+            elif vals[m][1] > timestamp:
+                r = m - 1
+            else:
+                return vals[m][0]
+        return res
+
+
 
 
 # Your TimeMap object will be instantiated and called as such:
