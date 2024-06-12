c296acef4efdd6c08bd3c3ab24c8eb436d0b61a4
RochaThomas <tomrocha99@gmail.com>
2024-06-12 09:02:33 -0700

    morning algos numberOf1Bits prac complete

--- numberOf1Bits.py
+++ numberOf1Bits.py
@@ -3,7 +3,16 @@
 
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        # bit manipulation
-        pass
+        res = 0
+        while n:
+            n &= (n - 1)
+            res += 1
+        return res
+
+        # res = 0
+        # while n:
+        #     res += n % 2
+        #     n = n >> 1
+        # return res
 
     # print(hammingWeight(n = 00000000000000000000000010000000))
