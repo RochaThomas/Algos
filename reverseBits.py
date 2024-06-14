7cb7c6c9d651ee2afb2e56aae12feb30f77e4c65
RochaThomas <tomrocha99@gmail.com>
2024-06-14 13:21:18 -0700

    morning algos reverseBits prac

--- reverseBits.py
+++ reverseBits.py
@@ -3,10 +3,4 @@
 
 class Solution:
     def reverseBits(self, n):
-        res = 0
-
-        for i in range(32):
-            bit = (n >> i) & 1
-            res = res | (bit << (31 - i))
-
-        return res
+        pass
