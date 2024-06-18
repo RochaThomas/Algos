09d821c503b82f962f9e7b5f22cec3c4c83edad3
RochaThomas <tomrocha99@gmail.com>
2024-06-18 10:28:22 -0700

    morning algos countingBits prac complete

--- countingBits.py
+++ countingBits.py
@@ -3,7 +3,12 @@
 
 class Solution:
     def countBits(self, n):
-        # bit manipulation
-        pass
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if offset * 2 == i:
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
 
     print(countBits(2))
