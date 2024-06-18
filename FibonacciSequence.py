b0ea178428bb5647ffc11101c40717af5c48eaab
RochaThomas <tomrocha99@gmail.com>
2024-06-18 16:11:16 -0700

    new prac algo FibonacciSeq prac complete

---
+++ FibonacciSequence.py
@@ -0,0 +1,14 @@
+# interview prep practice
+# Fibonacci Sequence
+class Solution:
+    def fibSeq(n):
+        res = [0, 1]
+        prev, curr = res[0], res[1]
+        for i in range(n):
+            temp = prev + curr
+            res.append(temp)
+            prev = curr
+            curr = temp
+        return res
+
+    print(fibSeq(10))
