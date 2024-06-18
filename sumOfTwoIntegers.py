3f8eec21215232923ba7a603c4e954d72c85384a
RochaThomas <tomrocha99@gmail.com>
2024-06-18 11:23:21 -0700

    morning algos sumOfTwoInt prac complete

--- sumOfTwoIntegers.py
+++ sumOfTwoIntegers.py
@@ -3,6 +3,10 @@
 
 class Solution:
     def getSum(self, a, b):
-        pass
+        while b != 0:
+            temp = (a & b) << 1
+            a = a ^ b
+            b = temp
+        return a
 
     print(getSum(1, 2))
