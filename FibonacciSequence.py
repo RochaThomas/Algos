bf0578efcf2c3868a13c33c106825dd60899cbd7
RochaThomas <tomrocha99@gmail.com>
2024-06-18 16:53:18 -0700

    recursion and cachimg fibSeq prac complete

--- FibonacciSequence.py
+++ FibonacciSequence.py
@@ -17,5 +17,27 @@ def recFibSeq(n):
         return 0
     else:
         return recFibSeq(n - 2) + recFibSeq(n - 1)
+    
+def recFibSeqCaching(n):
+    cache = {}
+    def helper(n):
+        if n in cache:
+            return cache[n]
+        elif n == 1:
+            value = 1
+        elif n == 0:
+            value = 0
+        else:
+            value = helper(n - 1) + helper(n - 2)
+        
+        cache[n] = value
+        return value
+    
+    res = []
+    for i in range(n + 1):
+        res.append(helper(i))
+            
+    helper(10)
+    return res
 
-print(recFibSeq(n=10))
+print(recFibSeqCaching(n=10))
